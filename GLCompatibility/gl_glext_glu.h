8ffc66c558dab6c7bd0fe18693f4236e8d281f03
Victor 'Yes, The Victor' Dods <vdods@leapmotion.com>
2014-07-25 16:26:19 -0700

    Added include for GLEW for Mac and other non WIN32 platforms (other non WIN32 platforms not build-tested).

--- GLCompatibility/gl_glext_glu.h
+++ GLCompatibility/gl_glext_glu.h
@@ -3,16 +3,16 @@
 // this header is only intended to take care of the annoying OpenGL header includes in a cross-platform way.
 
 #if __APPLE__
+  #include <GL/glew.h>
   #include <OpenGL/gl.h>
-  #include <OpenGL/glext.h> // for certain internal texture formats
   #include <OpenGL/glu.h>
 #elif _WIN32
   #define GLEW_STATIC
   #include <GL/glew.h>
   #include "windows.h"
 #else
+  #include <GL/glew.h>
   #include <gl/gl.h>
-  #include <gl/glext.h> // for certain internal texture formats
   #include <gl/glu.h>
 #endif
 
