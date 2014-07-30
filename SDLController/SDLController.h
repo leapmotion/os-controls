4a041d703d67177c244dc795e9a2e26a922dafd1
Victor 'Yes, The Victor' Dods <vdods@leapmotion.com>
2014-07-30 14:00:15 -0700

    Added design notes for an SDLControllerParams structure which would be used in SDLController::Initialize for finer and more powerful control.

--- SDLController/SDLController.h
+++ SDLController/SDLController.h
@@ -4,6 +4,32 @@
 
 #include <string>
 
+// Design ideas/notes for SDLControllerParams structure
+// ----------------------------------------------------
+// There are several parameters for configuring/initializing the SDL
+// window, renderer and GL context.  A clean way to provide fully
+// specified initialization configuration is to pass in a structure
+// containing the desired configuration parameters, call it SDLControllerParams.
+// There should be sensible defaults for some of the parameters, but
+// perhaps some of the parameters have no reasonable defaults and must
+// be specified (such as window size).
+//
+// Such a structure would have the following properties:
+// - Params to SDL_Init (currently we use SDL_INIT_TIMER, SDL_INIT_VIDEO, SDL_INIT_EVENTS,
+//   but maybe the user also wants SDL_INIT_AUDIO, SDL_INIT_NOPARACHUTE, etc).
+// - Transparency on or off
+// - Window location
+// - Window size
+// - Fullscreen on or off
+// - Params to SDL_CreateRenderer (currently we use SDL_RENDERER_ACCELERATED,
+//   SDL_RENDERER_PRESENTVSYNC, but maybe the user wants SDL_RENDERER_SOFTWARE, which
+//   actually would be what we would want on e.g. a unit test of the SDLController
+//   and other rendering capabilities).
+//
+// The default constructor of SDLControllerParams should initialize everything to a
+// reasonable default, and SDLController::Initialize should accept a single const
+// reference to SDLControllerParams, with a default value of SDLControllerParams().
+
 // This class bundles all the SDL usage/state into a single point of control.
 // TODO: think about renaming this to SDLController or something, because "context"
 // means something else in autowiring.
