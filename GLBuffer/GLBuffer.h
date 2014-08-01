ffb16f9e33a33f2ede0e32fbecfd3b9dfa6a4a12
Raffi Bedikian <rbedikian@leapmotion.com>
2014-08-01 13:46:03 -0700

    Update GLBuffer code style

--- GLBuffer/GLBuffer.h
+++ GLBuffer/GLBuffer.h
@@ -8,21 +8,22 @@ class GLBuffer {
 public:
 
   GLBuffer();
-  void create(GLenum type);
-  void bind();
-  void allocate(const void* data, int count, GLenum pattern);
-  void release();
-  int size() const;
-  void* map(GLuint access);
-  bool unmap();
-  bool isCreated() const;
-  void destroy();
-  static void checkError(const std::string& loc = "");
-  static void checkFrameBufferStatus(const std::string& loc = "");
+  void Create(GLenum type);
+  void Bind();
+  void Allocate(const void* data, int count, GLenum pattern);
+  void Release();
+  int Size() const;
+  void* Map(GLuint access);
+  bool Unmap();
+  bool IsCreated() const;
+  void Destroy();
+
+  static void CheckError(const std::string& loc = "");
+  static void CheckFrameBufferStatus(const std::string& loc = "");
 
 private:
 
-  GLuint buffer_;
-  GLenum type_;
+  GLuint m_BufferAddress;
+  GLenum m_BufferType;
 
 };
