ee9680b109cb2de437f751fd00becf44d736c5e2
Jason Lokerson <cmercenary@gmail.com>
2014-08-01 21:01:37 -0700

    Initialize COM in unit tests

--- src/osinterface/test/osinterfacetest.cpp
+++ src/osinterface/test/osinterfacetest.cpp
@@ -1,8 +1,12 @@
 // Copyright (C) 2012-2014 Leap Motion, Inc. All rights reserved.
 #include "stdafx.h"
 #include <autowiring/gtest-all-guard.h>
+#include "utility/ComInitializer.h"
 
 int main(int argc, const char* argv[])
 {
+  // Will need COM for some tests, might as well initialize it everywhere
+  ComInitializer initCom;
+
   return autotesting_main(argc, argv);
 }
