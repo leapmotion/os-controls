27331d255ed18de422c31a11a9d022b0724739bc
Walter Gray <wgray@leapmotion.com>
2014-08-05 16:20:45 -0700

    Renamed the engine project to the graphics project
    Moved the core OSControl class from OSInteraction into oscontrols

--- src/oscontrols/oscontrols.cpp
+++ src/oscontrols/oscontrols.cpp
@@ -1,10 +1,83 @@
 #include "stdafx.h"
-#include "osinterface/osinterface.h"
+#include "oscontrols.h"
+#include "osinterface/AudioVolumeController.h"
+#include "osinterface/LeapInput.h"
+#include "osinterface/MainWindow.h"
+#include "osinterface/MediaController.h"
+#include "osinterface/SdlInitializer.h"
+#include "utility/ComInitializer.h"
 #include <SDL.h>
 
 int main(int argc, char **argv)
 {
-  osinterface_main(argc, argv);
+  AutoCurrentContext ctxt;
+  ctxt->Initiate();
+
+  try {
+    AutoRequired<OsControl> control;
+    control->Main();
+  }
+  catch (...) {
+    return -1;
+  }
+
+  ctxt->SignalShutdown(true);
   return 0;
 }
 
+OsControl::OsControl(void) :
+m_mw(""),
+m_bShouldStop(false),
+m_bRunning(false)
+{}
+
+void OsControl::Main(void) {
+  ComInitializer initCom;
+  auto clearOutstanding = MakeAtExit([this] {
+    std::lock_guard<std::mutex> lk(m_lock);
+    m_outstanding.reset();
+    m_stateCondition.notify_all();
+  });
+
+  AutoRequired<AutoPacketFactory> factory;
+
+  // Dispatch events until told to quit:
+  while (!ShouldStop()) {
+    SDL_Event ev;
+    while (SDL_PollEvent(&ev))
+      HandleEvent(ev);
+
+    // Pilot a packet through the system: 
+    auto packet = factory->NewPacket();
+  }
+}
+
+void OsControl::HandleEvent(const SDL_Event& ev) const {
+  switch (ev.type) {
+  case SDL_QUIT:
+    AutoCurrentContext()->SignalShutdown();
+    break;
+  }
+}
+
+void OsControl::Filter(void) {
+  try {
+    throw;
+  }
+  catch (std::exception& ex) {
+    std::cerr << ex.what() << std::endl;
+  }
+}
+
+bool OsControl::Start(std::shared_ptr<Object> outstanding) {
+  std::lock_guard<std::mutex> lk(m_lock);
+  if (m_bShouldStop)
+    return true;
+  m_outstanding = outstanding;
+  return true;
+}
+
+void OsControl::Wait(void) {
+  std::unique_lock<std::mutex> lk(m_lock);
+  m_stateCondition.wait(lk, [this] { return m_outstanding.get() == nullptr; });
+}
--- src/oscontrols/oscontrols.h
+++ src/oscontrols/oscontrols.h
@@ -1,1 +1,50 @@
 #pragma once
+#include <autowiring/autowiring.h>
+
+class AudioVolumeController;
+class LeapInput;
+class MainWindow;
+class MediaController;
+class SdlInitializer;
+union SDL_Event;
+
+int oscontrols_main(int argc, char **argv);
+
+class OsControl :
+  public CoreRunnable,
+  public ExceptionFilter
+{
+public:
+  OsControl(void);
+
+private:
+  std::mutex m_lock;
+  std::condition_variable m_stateCondition;
+
+  AutoRequired<SdlInitializer> m_initializer;
+  AutoDesired<AudioVolumeController> m_avcontrol;
+  AutoConstruct<MainWindow> m_mw;
+  AutoRequired<MediaController> m_media;
+  AutoRequired<LeapInput> m_leapInput;
+
+  // Requirements for this to be a CoreRunnable:
+  bool m_bShouldStop;
+  bool m_bRunning;
+  std::shared_ptr<Object> m_outstanding;
+
+  /// <summary>
+  /// Handles an SDL event from the primary event dispatch loop
+  /// </summary>
+  void HandleEvent(const SDL_Event& ev) const;
+
+public:
+  void Main(void);
+  void Filter(void);
+
+  // CoreRunnable overrides
+  bool Start(std::shared_ptr<Object> outstanding) override;
+  void Stop(bool graceful) override { m_bShouldStop = true; }
+  bool IsRunning(void) const override { return m_bRunning; }
+  bool ShouldStop(void) const override { return m_bShouldStop; }
+  void Wait(void) override;
+};
