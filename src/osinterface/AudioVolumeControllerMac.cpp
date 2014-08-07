e9da5cf6ff4f8614d365e0dccc96dc2abc692ea7
Jonathan Marsden <jmarsden@leapmotion.com>
2014-08-07 16:01:28 -0700

    Add support for adjusting volume on Mac

--- src/osinterface/AudioVolumeControllerMac.cpp
+++ src/osinterface/AudioVolumeControllerMac.cpp
@@ -9,9 +9,53 @@ AudioVolumeController* AudioVolumeController::New(void) {
 }
 
 float AudioVolumeControllerMac::GetVolume(void) {
-  float level = 0;
-  return level;
+  float volume = 0;
+  AudioDeviceID audioDevice = GetAudioDeviceID();
+  // Try to get the master channel. If it fails, try to then get left channel and right channel
+  for (AudioObjectPropertyElement channel = 0; channel < 3; channel++) {
+    AudioObjectPropertyAddress propertyAddress = { kAudioDevicePropertyVolumeScalar,
+                                                   kAudioDevicePropertyScopeOutput,
+                                                   channel };
+    Float32 theVolume = 0;
+    if (AudioObjectHasProperty(audioDevice, &propertyAddress)) {
+      UInt32 size = sizeof(Float32);
+      AudioObjectGetPropertyData(audioDevice, &propertyAddress, 0, nullptr, &size, &theVolume);
+      float channelVolume = static_cast<float>(theVolume);
+      if (channelVolume > volume) {
+        volume = channelVolume;
+      }
+      if (channel == 0) { // If master channel, nothing left to do
+        break;
+      }
+    }
+  }
+  return volume;
 }
 
 void AudioVolumeControllerMac::SetVolume(float volume) {
+  AudioDeviceID audioDevice = GetAudioDeviceID();
+  // Try to set the master channel. If it fails, try to then set left channel and right channel
+  for (AudioObjectPropertyElement channel = 0; channel < 3; channel++) {
+    AudioObjectPropertyAddress propertyAddress = { kAudioDevicePropertyVolumeScalar,
+                                                   kAudioDevicePropertyScopeOutput,
+                                                   channel };
+    if (AudioObjectHasProperty(audioDevice, &propertyAddress)) {
+      Float32 theVolume = static_cast<Float32>(volume);
+      UInt32 size = sizeof(Float32);
+      AudioObjectSetPropertyData(audioDevice, &propertyAddress, 0, nullptr, size, &theVolume);
+      if (channel == 0) { // If master channel, nothing left to do
+        break;
+      }
+    }
+  }
+}
+
+AudioDeviceID AudioVolumeControllerMac::GetAudioDeviceID() {
+  AudioObjectPropertyAddress outputDeviceAddress = { kAudioHardwarePropertyDefaultOutputDevice,
+                                                     kAudioObjectPropertyScopeGlobal,
+                                                     kAudioObjectPropertyElementMaster };
+  AudioDeviceID outputDeviceID = 0;
+  UInt32 size = sizeof(AudioDeviceID);
+  AudioObjectGetPropertyData(kAudioObjectSystemObject, &outputDeviceAddress, 0, nullptr, &size, &outputDeviceID);
+  return outputDeviceID;
 }
