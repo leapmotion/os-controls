a6294a37b33d071789f3e3ee68af45f39e24e6fa
Jason Lokerson <cmercenary@gmail.com>
2014-08-07 11:56:03 -0700

    Replacing autowiring selective merge with a proper subtree merge

--- contrib/autowiring/contrib/C++11/boost_thread.h
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2012-2014 Leap Motion, Inc. All rights reserved.
-#pragma once
-
-#include <boost/thread/thread.hpp>
-
-namespace std {
-  using boost::thread;
-  namespace this_thread = boost::this_thread;
-}
--- contrib/autowiring/src/autowiring/CoreContextStateBlock.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2012-2014 Leap Motion, Inc. All rights reserved.
-#include "stdafx.h"
-#include "CoreContextStateBlock.h"
-
-CoreContextStateBlock::CoreContextStateBlock(void){}
-
-CoreContextStateBlock::~CoreContextStateBlock(void){}
