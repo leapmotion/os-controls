643aa9f0e97229dd5bc1a5a648ffbf6d5a2820ea
Graham Tremper <gtremper@leapmotion.com>
2014-08-12 15:33:54 -0700

    Removed unnecessary parts of gtest

--- C++11/Test/C++11Compliance.cpp
+++ C++11/Test/C++11Compliance.cpp
@@ -1,19 +1,28 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
-class CPPComplianceTest : public testing::Test { };
+class CPPComplianceTest:
+  public testing::Test
+{};
 
 // Verifying that variadic templates work.
 
 void PrintArgs () { }
 
-template <typename T, typename... Types>
-void PrintArgs (const T &arg, Types... args) {
-  std::cout << arg << " ";
+template <typename T>
+void PrintArgs (const T &arg) {
+  std::cout << arg << std::endl;
+}
+
+template <typename T1, typename T2, typename... Types>
+void PrintArgs (const T1 &arg1, const T2 &arg2, Types... args) {
+  std::cout << arg1 << " "<< arg2 << " ";
   PrintArgs(args...);
 }
 
+
 TEST_F(CPPComplianceTest, VariadicTemplate) {
-  PrintArgs(1234567, 42.0f, "hello");
-  std::cout << '\n';
+  PrintArgs(1234567, 42.0f, "hello", false);
+
+  ASSERT_EQ(42, 42) << "What's the question?";
 }
