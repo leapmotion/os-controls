c61f4bc27ae432f9e41350c802217554c18f470e
Walter Gray <wgray@leapmotion.com>
2014-08-13 14:12:18 -0700

    Removed SFMLControler dependency from TextFileLoader, made it just use relative paths.

--- TextAndBinaryFileLoader/TextFileLoader.h
+++ TextAndBinaryFileLoader/TextFileLoader.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "ResourceManager.h"
-#include "SFMLController.h"
 #include "TextFile.h"
 
 // Template specialization of ResourceLoader<TextFile> which defines how to load such a resource.
@@ -11,7 +10,7 @@ struct ResourceLoader<TextFile> {
   static std::shared_ptr<TextFile> LoadResource (const std::string &name, ResourceManager<TextFile> &calling_manager) {
     // TODO: do real filesystem path lookup, or have some sort of configuration singleton that has this path
     try {
-      return std::make_shared<TextFile>(SFMLController::BasePath() + name);
+      return std::make_shared<TextFile>(name);
     } catch (const std::exception &e) {
       // change the generic exception into a type-specific one.
       throw ResourceExceptionOfType<TextFile>(e.what());
