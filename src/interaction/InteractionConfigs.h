61d4f5f65e0ab30de1fc5158bd18c91e21ea4e31
Daniel Plemmons <dpx109@gmail.com>
2014-08-19 13:20:10 -0700

    update interaction box y

--- src/interaction/InteractionConfigs.h
+++ src/interaction/InteractionConfigs.h
@@ -10,7 +10,7 @@
 #include "EigenTypes.h"
 
 namespace config {
-  const Vector3 m_leapMin(-120.0f,200.0f,-30.0f);
+  const Vector3 m_leapMin(-120.0f,300.0f,-30.0f);
   const Vector3 m_leapMax(120.0f,600.0f,30.0f);
   const float MIN_DOT_FOR_POINTING = 0.75f;
   const float MAX_DOT_FOR_CURLED = 0.1f;
