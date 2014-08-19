7357277bc401a8b1cf69ccdd19f3fbbbaad52ad9
Walter Gray <wgray@leapmotion.com>
2014-08-19 12:54:02 -0700

    added missing includes to Animation.h

--- Animation/Animation.h
+++ Animation/Animation.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <cassert>
+#include <cmath>
 
 // This is a simple animation utility class that will perform Poisson smoothing.
 // The class is templated and can be used with double, float, Vector3, or anything
