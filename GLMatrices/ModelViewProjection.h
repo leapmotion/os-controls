98d8eec5118e766b859a98eec3318e0038d6c96c
Victor 'Yes, The Victor' Dods <vdods@leapmotion.com>
2014-08-19 23:22:16 -0700

    Took the dependence on OpenGL out of ModelView and Projection, as the gl matrix stack functions are deprecated, and we shovel matrix data into shaders now.

--- GLMatrices/ModelViewProjection.h
+++ GLMatrices/ModelViewProjection.h
@@ -1,19 +1,15 @@
 #pragma once
 
-#include "gl_glext_glu.h"
 #include "EigenTypes.h"
 
 class Projection {
 public:
   Projection();
   const Matrix4x4& Matrix() const;
-  void LoadFromCurrent();
-  void SetCurrent();
   void Perspective(double left, double bottom, double right, double top, double nearClip, double farClip);
   void Perspective(double hFovRadians, double widthOverHeight, double nearClip, double farClip);
   void Orthographic(double left, double bottom, double right, double top, double nearClip, double farClip);
   Vector2 Project(const Vector3& point) const;
-  void SetUniform(int address) const;
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 private:
   Matrix4x4 m_matrix;
@@ -23,8 +19,6 @@ class ModelView {
 public:
   ModelView();
   const Matrix4x4& Matrix() const;
-  void LoadFromCurrent();
-  void SetCurrent();
   void Reset();
   void LookAt(const Vector3& eye, const Vector3& center, const Vector3& up);
   void Translate(const Vector3& translation);
@@ -35,7 +29,6 @@ public:
   //   [ M 0 ]
   //   [ 0 1 ]
   void Multiply(const Matrix3x3& transform);
-  void SetUniform(int address) const;
   void Push();
   void Pop();
 private:
