55984d8b85b08b95646ded113fe7683658c9ecd7
Jason Lokerson <cmercenary@gmail.com>
2014-08-21 15:32:36 -0700

    Sample refactor of the HandPointingDecorator class

--- src/interaction/HandPointingDecorator.h
+++ src/interaction/HandPointingDecorator.h
@@ -1,11 +1,8 @@
 #pragma once
-
 #include "Leap.h"
 #include "EigenTypes.h"
 #include <Eigen/Dense>
 #include <map>
-
-#include <autowiring/Autowired.h>
 
 struct HandPointMap : public std::map<int32_t, Leap::Hand> {};
 struct HandPoseVector : public std::vector<HandPointMap> {};
