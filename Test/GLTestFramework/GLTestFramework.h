7b464a32a30531af27fb4f3389d4b91c87e3c8f9
Victor 'Yes, The Victor' Dods <vdods@leapmotion.com>
2014-08-23 04:08:18 -0700

    Corrected the locations of some GLTestFramework method definitions that were in a header instead of an implementation file.

--- Test/GLTestFramework/GLTestFramework.h
+++ Test/GLTestFramework/GLTestFramework.h
@@ -10,14 +10,8 @@ protected:
 
   enum class Visibility { VISIBLE, HEADLESS };
   
-  GLTestFramework (Visibility visibility) {
-    if (visibility == Visibility::HEADLESS) {
-      // Cause the window to not be shown.
-      m_SDLControllerParams.windowFlags &= ~SDL_WINDOW_SHOWN;  // Disable the "shown" flag.
-      m_SDLControllerParams.windowFlags |=  SDL_WINDOW_HIDDEN; // Enable the "hidden" flag.
-    }
-  }
-  virtual ~GLTestFramework () { }
+  GLTestFramework (Visibility visibility);
+  virtual ~GLTestFramework ();
   
   virtual void SetUp () override;
   virtual void TearDown () override;
@@ -28,17 +22,6 @@ protected:
   SDLController m_SDLController;
   GLController m_GLController;
 };
-
-void GLTestFramework::SetUp () {
-  m_SDLController.Initialize(m_SDLControllerParams);
-  m_GLController.Initialize();
-}
-
-void GLTestFramework::TearDown () {
-  // Shut down the created things in reverse order
-  m_GLController.Shutdown();
-  m_SDLController.Shutdown();
-}
 
 // For creating a GLTestFramework that renders to a visible window
 class GLTestFramework_Visible : public GLTestFramework {
