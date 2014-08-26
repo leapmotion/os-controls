c3afd596ca80e8d2ed2ff29c6e2a89267e8379ff
Jason Lokerson <cmercenary@gmail.com>
2014-08-26 09:52:33 -0700

    Unit test implemented to demonstrate baseline capabilities of OSWindowManagerInterface

--- src/osinterface/test/OSWindowInterfaceTest.cpp
+++ src/osinterface/test/OSWindowInterfaceTest.cpp
@@ -8,7 +8,7 @@ class OSWindowInterfaceTest:
 
 TEST_F(OSWindowInterfaceTest, CanEnumerateTopLevelWindows) {
   AutoRequired<WindowCreatingTestFixture> wctf;
-  auto props = wctf->CreateTestWindow();
+  const auto testWindowProps = wctf->CreateTestWindow();
 
   // Now see if we can enumerate to this window as one of the top-level windows:
   AutoRequired<OSWindowManagerInterface> oswmi;
@@ -16,4 +16,17 @@ TEST_F(OSWindowInterfaceTest, CanEnumerateTopLevelWindows) {
 
   // At the minimum, we should find one top-level window
   ASSERT_FALSE(windows.empty()) << "Child enumeration failed to return a single window";
+
+  // Now try to find our window:
+  auto q = std::find_if(
+      windows.begin(),
+      windows.end(),
+      [&testWindowProps] (const std::shared_ptr<OSWindowNode>& node) {
+        return node->GetOwnerPid() == testWindowProps.pid;
+      }
+    );
+  ASSERT_NE(windows.end(), q) << "Failed to find the test window in the enumeration of top-level windows";
+
+  // Validate names are what we expect to find:
+  std::shared_ptr<OSWindowNode> foundTestWindow = *q;
 }
