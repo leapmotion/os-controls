10b44b1a9977622dc06890e339afdec94cdfbf8f
Raffi Bedikian <rbedikian@leapmotion.com>
2014-09-02 11:51:47 -0700

    Fix some more minor Eigen issues

--- src/interaction/CoordinateUtility.h
+++ src/interaction/CoordinateUtility.h
@@ -10,7 +10,7 @@ class CoordinateUtility {
 public:
   Vector2 LeapToScreen(Leap::Vector leapCoords);
   Vector3 LeapToScreenNormal(Leap::Vector leapCoords);
-  Vector2 ScreenNormalToScreenPixels(Vector3 normalCoords);
+  Vector2 ScreenNormalToScreenPixels(const Vector3& normalCoords);
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 private:
   Autowired<sf::RenderWindow> m_window;
