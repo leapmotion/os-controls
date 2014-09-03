498fbf48bd5940f0ea89de4763ca5446b8517a75
Jonathan Marsden <jmarsden@leapmotion.com>
2014-09-03 10:33:18 -0700

    Fix problem with Mac initial window positioning on multi-monitor system

--- src/osinterface/MakesRenderWindowFullScreen.cpp
+++ src/osinterface/MakesRenderWindowFullScreen.cpp
@@ -4,7 +4,9 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-MakesRenderWindowFullScreen::MakesRenderWindowFullScreen(void) {}
+MakesRenderWindowFullScreen::MakesRenderWindowFullScreen(void) {
+  *this += [this] { AdjustDesktopWindow(); };
+}
 MakesRenderWindowFullScreen::~MakesRenderWindowFullScreen(void) {}
 
 void MakesRenderWindowFullScreen::AdjustDesktopWindow(void) {
@@ -16,14 +18,21 @@ void MakesRenderWindowFullScreen::AdjustDesktopWindow(void) {
   const sf::Vector2u oldSize = m_mw->getSize();
 
   const auto bounds = m_virtualScreen->PrimaryScreen().Bounds();
-  const sf::Vector2i newPosition = {static_cast<int32_t>(bounds.origin.x),
-    static_cast<int32_t>(bounds.origin.y)};
-  const sf::Vector2u newSize = {static_cast<uint32_t>(bounds.size.width),
-    static_cast<uint32_t>(bounds.size.height)};
+  const sf::Vector2i newPosition{static_cast<int32_t>(bounds.origin.x), static_cast<int32_t>(bounds.origin.y)};
+  const sf::Vector2u newSize{static_cast<uint32_t>(bounds.size.width), static_cast<uint32_t>(bounds.size.height)};
 
   if(oldSize != newSize) {
     m_mw->create(sf::VideoMode(newSize.x, newSize.y), "Leap Os Control", sf::Style::None, *m_contextSettings);
   }
+#if __APPLE__
+  // The SFML code for positioning a window on Mac OS X incorrectly uses the
+  // height of the screen containing the window to flip the Y coordinates. It
+  // should always use the height of the default screen. To workaround this
+  // shortcoming, first set position of this window to the origin. It should
+  // hopefully be on the default screen, but if the height differences between
+  // screens is too far off, it could potentially miss.
+  m_mw->setPosition(sf::Vector2i{0,0});
+#endif
   m_mw->setPosition(newPosition);
   const auto handle = m_mw->getSystemHandle();
   NativeWindow::MakeTransparent(handle);
@@ -31,4 +40,3 @@ void MakesRenderWindowFullScreen::AdjustDesktopWindow(void) {
   NativeWindow::AllowInput(handle, false);
   m_mw->setVisible(true);
 }
-
