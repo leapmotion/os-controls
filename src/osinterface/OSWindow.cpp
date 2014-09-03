c89bc8a37c4452b44129917655c8106cde788924
Jason Lokerson <cmercenary@gmail.com>
2014-09-03 10:57:15 -0700

    Correcting out-of-bounds reference

--- src/osinterface/OSWindow.cpp
+++ src/osinterface/OSWindow.cpp
@@ -20,19 +20,25 @@ void OSWindow::GetWindowTexture(ImagePrimitive& img) {
   size_t cy = (size_t) ceil(sz.height);
   std::vector<uint32_t> sampleSpace(cx * cy);
 
+  if(!cx || !cy)
+    // No image, nothing to be drawn--leave the image empty
+    return;
+
   // Fill with solid bands:
   uint32_t colors[] = {0xFFFF00FF, 0xFFFF0000, 0xFF0000FF, 0xFF00FF00};
   for(size_t i = cy; i--;) {
     std::fill(
-      &sampleSpace[i * cx],
-      &sampleSpace[(i + 1) * cx],
+      sampleSpace.data() + i * cx,
+      sampleSpace.data() + (i + 1) * cx,
       colors[(i / 3) % 4]
     );
   }
 
   // Create a new texture if we have to:
   auto tex = img.Texture();
-  GLTexture2Params params = tex->Params();
+  GLTexture2Params params;
+  if(tex)
+    params = tex->Params();
   if(
     cx != params.Width() ||
     cy != params.Width()
