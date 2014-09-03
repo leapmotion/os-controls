74efae3109824e3a753ebad5cb9aab46f7a19ba8
Jonathan Marsden <jmarsden@leapmotion.com>
2014-09-03 08:12:19 -0700

    Add missing override

--- src/osinterface/WindowScroller.h
+++ src/osinterface/WindowScroller.h
@@ -41,7 +41,7 @@ protected:
 
   // IScrollOperation overrides:
   void ScrollBy(const OSPoint& virtualPosition, float deltaX, float deltaY) override final;
-  void CancelScroll(void) final;
+  void CancelScroll(void) override final;
 
 private:
   // Total current momentum caused by recently received scroll operations.  This momentum is in
