54377643f6a21604d00f505a869d360da1ab2960
Daniel Plemmons <dpx109@gmail.com>
2014-09-05 15:59:57 -0700

    fix knob color rendering

--- src/graphics/volumeKnob.cpp
+++ src/graphics/volumeKnob.cpp
@@ -7,13 +7,9 @@ VolumeKnob::VolumeKnob() {
   
   m_knobIndicator.SetSize(Vector2(2.0f, 80.0));
   m_knobIndicator.Translation() = Vector3(0, 30, 0);
-  m_knobIndicator.Material().SetAmbientLightColor(Color(255.0f, 255.0f, 255.0f));
-  //m_knobIndicator.Material().SetDiffuseLightColor(Color(100.0f, 255.0f, 100.0f, 1.0f));
   
   m_knobBody.SetRadius(40.0f);
   m_knobIndicator.Translation() = Vector3(0, 0, 0);
-  m_knobBody.Material().SetAmbientLightColor(Color(128.0f, 128.0f, 128.0f));
-  //m_knobBody.Material().SetDiffuseLightColor(Color(128.0f, 128.0f, 128.0f, 0.7f));
 }
 
 VolumeKnob::~VolumeKnob() {
@@ -23,12 +19,14 @@ VolumeKnob::~VolumeKnob() {
 void VolumeKnob::Tick(std::chrono::duration<double> deltaT) {
   m_opacity.Update(deltaT.count());
   
-  const Color knobColor(65.0f, 65.0f, 65.0f, m_opacity.Value());
-  const Color indicatiorColor(20.0f, 255.0f, 20.0f, m_opacity.Value());
+  const Color knobColor(0.5f, 0.5f, 0.5f, m_opacity.Value());
+  const Color indicatiorColor(0.5f, 1.0f, 0.5f, m_opacity.Value());
   
+  m_knobBody.Material().SetAmbientLightingProportion(1.0f);
   m_knobBody.Material().SetDiffuseLightColor(knobColor);
   m_knobBody.Material().SetAmbientLightColor(knobColor);
   
+  m_knobIndicator.Material().SetAmbientLightingProportion(1.0f);
   m_knobIndicator.Material().SetDiffuseLightColor(indicatiorColor);
   m_knobIndicator.Material().SetAmbientLightColor(indicatiorColor);
 }
