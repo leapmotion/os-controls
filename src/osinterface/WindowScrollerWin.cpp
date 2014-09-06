dca2fdbb2ee34f60c38738a67f9bfeb39b6ac2a5
Jason Lokerson <cmercenary@gmail.com>
2014-09-05 17:10:10 -0700

    Adding a lines-per-click conversion ratio

--- src/osinterface/WindowScrollerWin.cpp
+++ src/osinterface/WindowScrollerWin.cpp
@@ -10,10 +10,13 @@ WindowScrollerWin::WindowScrollerWin() :
 {
   // Set pixels per line to the number of lines per scroll wheel notch
   UINT param = 0;
-  m_pixelsPerLine.x =
+  m_linesPerClick.x =
     (SystemParametersInfo(SPI_GETWHEELSCROLLCHARS, 0, &param, 0) && (param != 0)) ? static_cast<float>(param) : 3;
-  m_pixelsPerLine.y =
+  m_linesPerClick.y =
     (SystemParametersInfo(SPI_GETWHEELSCROLLLINES, 0, &param, 0) && (param != 0)) ? static_cast<float>(param) : 3;
+
+  m_pixelsPerLine.x = 12;
+  m_pixelsPerLine.y = 12;
 }
 
 WindowScrollerWin::~WindowScrollerWin() {
@@ -21,29 +24,33 @@ WindowScrollerWin::~WindowScrollerWin() {
 }
 
 void WindowScrollerWin::DoScrollBy(float deltaX, float deltaY, bool isMomentum) {
-  const OSPoint deltaPixel = OSPointMake(deltaX, deltaY);
-  OSPoint deltaLine  = OSPointMake(deltaPixel.x/m_pixelsPerLine.x, deltaPixel.y/m_pixelsPerLine.y); // Convert to lines
+  // Convert to lines
+  OSPoint wheelDelta{
+    WHEEL_DELTA * (deltaX / m_linesPerClick.x) / m_pixelsPerLine.x,
+    WHEEL_DELTA * (deltaY / m_linesPerClick.y) / m_pixelsPerLine.y
+  };
 
   // Adjust partial lines
-  m_scrollPartialLine.x += deltaLine.x;
-  m_scrollPartialLine.y += deltaLine.y;
-  deltaLine.x = floor(m_scrollPartialLine.x);
-  deltaLine.y = floor(m_scrollPartialLine.y);
-  m_scrollPartialLine.x -= deltaLine.x;
-  m_scrollPartialLine.y -= deltaLine.y;
+  m_scrollPartialLine.x += wheelDelta.x;
+  m_scrollPartialLine.y += wheelDelta.y;
+  wheelDelta.x = floor(m_scrollPartialLine.x);
+  wheelDelta.y = floor(m_scrollPartialLine.y);
+  m_scrollPartialLine.x -= wheelDelta.x;
+  m_scrollPartialLine.y -= wheelDelta.y;
 
-  if (deltaLine.y != 0) {
+  if (wheelDelta.y != 0) {
     INPUT input = { 0 };
     input.type = INPUT_MOUSE;
     input.mi.dwFlags = MOUSEEVENTF_WHEEL;
-    input.mi.mouseData = static_cast<DWORD>(deltaLine.y*2); // Scale the step (Windows often drops values of -1 and 1)
+    input.mi.mouseData = static_cast<DWORD>(wheelDelta.y); // Scale the step (Windows often drops values of -1 and 1)
     SendInput(1, &input, sizeof(input));
   }
-  if (deltaLine.x != 0) {
+
+  if (wheelDelta.x != 0) {
     INPUT input = { 0 };
     input.type = INPUT_MOUSE;
     input.mi.dwFlags = MOUSEEVENTF_HWHEEL;
-    input.mi.mouseData = static_cast<DWORD>(-deltaLine.x*2); // Scale the step (see above); Also reverse direction of scroll
+    input.mi.mouseData = static_cast<DWORD>(-wheelDelta.x); // Scale the step (see above); Also reverse direction of scroll
     SendInput(1, &input, sizeof(input));
   }
 }
