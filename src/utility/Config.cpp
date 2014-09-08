bdc6aaf6aae57bef46291ace216f1f00f030fd8b
Walter Gray <wgray@leapmotion.com>
2014-09-08 15:39:34 -0700

    Updated Utility/Config.h/cpp to read & write json objects.

--- src/utility/Config.cpp
+++ src/utility/Config.cpp
@@ -1,6 +1,27 @@
 #include "stdafx.h"
 #include "Config.h"
 
+#include "autowiring/../contrib/json11/json11.hpp"
+#include <fstream>
+
 Config::Config(void) {
 
+}
+
+void Config::Save(const std::string& filename) const {
+  auto data = m_data.dump();
+
+  std::ofstream outFile(filename);
+  outFile << data;
+}
+
+void Config::Load(const std::string& filename) {
+  std::ifstream inFile(filename);
+  std::string err;
+  std::string data;
+
+  inFile >> data;
+  m_data = json11::Json::parse(data, err);
+  if (!err.empty())
+    throw std::runtime_error(std::string("Json parsing error:") + err);
 }
--- src/utility/Config.h
+++ src/utility/Config.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include<string>
+#include<map>
+#include<stdexcept>
+
+#include <autowiring/../contrib/json11/json11.hpp>
+
 /// <summary>
 /// Stores application-wide settings on a per-user basis
 /// </summary>
@@ -7,6 +13,78 @@ class Config {
 public:
   Config(void);
 
-public:
+  void Save(const std::string& filename = "config.json") const;
+  void Load(const std::string& filename = "config.json");
+
+  template<typename T>
+  T Get(const std::string& prop) const { static_assert("Unspecialized on type"); }
+
+  bool Exists(const std::string& prop) const {
+    return m_data.object_items().count(prop) > 0;
+  }
+
+  json11::Json::object::const_iterator GetRef(const std::string& prop) const {
+    return m_data.object_items().find(prop);
+  }
+
   std::chrono::microseconds GetFrameRate(void) const;
+
+private:
+  json11::Json m_data;
 };
+
+//Specializations for valid storage types
+
+template<>
+double Config::Get<double>(const std::string& prop) const {
+  auto& val = m_data[prop];
+  if (!val.is_number())
+    throw std::runtime_error(prop + "Is not a number");
+
+  return val.number_value();
+}
+
+template<>
+float Config::Get<float>(const std::string& prop) const {
+  auto& val = m_data[prop];
+  if (!val.is_number())
+    throw std::runtime_error(prop + "Is not a number");
+
+  return static_cast<float>(val.number_value());
+}
+
+template<>
+bool Config::Get<bool>(const std::string& prop) const {
+  auto& val = m_data[prop];
+  if (!val.is_bool())
+    throw std::runtime_error(prop + "Is not a bool");
+
+  return val.bool_value();
+}
+
+template<>
+const std::string& Config::Get<const std::string&>(const std::string& prop) const {
+  auto& val = m_data[prop];
+  if (!val.is_string())
+    throw std::runtime_error(prop + "Is not a string");
+
+  return val.string_value();
+}
+
+template<>
+const json11::Json::array& Config::Get<const json11::Json::array&>(const std::string& prop) const {
+  auto& val = m_data[prop];
+  if (!val.is_array())
+    throw std::runtime_error(prop + "Is not an array");
+
+  return val.array_items();
+}
+
+template<>
+const json11::Json::object& Config::Get<const json11::Json::object&>(const std::string& prop) const {
+  auto& val = m_data[prop];
+  if (!val.is_object())
+    throw std::runtime_error(prop + "Is not an object");
+
+  return val.object_items();
+}
