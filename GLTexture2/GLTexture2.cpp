09c06fc7d680b723d460cddacfe51e0ce18ea3f5
Victor 'Yes, The Victor' Dods <vdods@leapmotion.com>
2014-09-08 21:43:43 -0700

    Moved GLTexture2Params out into its own source files, as GLTexture2.h and GLTexture2.cpp was getting crowded.

--- GLTexture2/GLTexture2.cpp
+++ GLTexture2/GLTexture2.cpp
@@ -18,48 +18,6 @@ void ThrowOnGLError (const std::string &while_doing) {
 }
 
 // ////////////////////////////////////////////////////////////////////////////////////////////////
-// GLTexture2Params
-// ////////////////////////////////////////////////////////////////////////////////////////////////
-
-GLTexture2Params::GLTexture2Params (GLsizei width, GLsizei height)
-  :
-  m_target(DEFAULT_TARGET),
-  m_internal_format(DEFAULT_INTERNAL_FORMAT)
-{ 
-  m_size[0] = width;
-  m_size[1] = height;
-}
-
-GLfloat GLTexture2Params::TexParameterf (GLenum pname) const {
-  GLTexParameterfMap::const_iterator it = m_tex_parameter_f.find(pname);
-  if (it == m_tex_parameter_f.end()) {
-    throw std::domain_error("specified GLfloat-valued texture parameter not found and/or specified");
-  }
-  return it->second;
-}
-
-GLint GLTexture2Params::TexParameteri (GLenum pname) const {
-  GLTexParameteriMap::const_iterator it = m_tex_parameter_i.find(pname);
-  if (it == m_tex_parameter_i.end()) {
-    throw std::domain_error("specified GLint-valued texture parameter not found and/or specified");
-  }
-  return it->second;
-}
-
-void GLTexture2Params::SetTexParameterf (GLenum pname, GLfloat value) {
-  if (m_tex_parameter_i.find(pname) != m_tex_parameter_i.end()) {
-    throw std::domain_error("specified GLfloat-valued texture parameter already set in the GLint-valued parameters");
-  }
-  m_tex_parameter_f[pname] = value;
-}
-void GLTexture2Params::SetTexParameteri (GLenum pname, GLint value) {
-  if (m_tex_parameter_f.find(pname) != m_tex_parameter_f.end()) {
-    throw std::domain_error("specified GLint-valued texture parameter already set in the GLfloat-valued parameters");
-  }
-  m_tex_parameter_i[pname] = value;
-}
-
-// ////////////////////////////////////////////////////////////////////////////////////////////////
 // GLTexture2PixelData
 // ////////////////////////////////////////////////////////////////////////////////////////////////
 
