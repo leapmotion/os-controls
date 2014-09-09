96e15b7f326d553e2e79da920e187416b3a8c692
Walter Gray <wgray@leapmotion.com>
2014-09-09 16:03:08 -0700

    Fixed a series of double->float coercion warnings

--- src/interaction/HandActivationRecognizer.h
+++ src/interaction/HandActivationRecognizer.h
@@ -23,10 +23,10 @@ public:
   
   void AutoFilter(const Leap::Hand& hand, const FrameTime& frameTime, HandGrab& handGrab, HandPinch& handPinch);
 private:
-  const float MIN_GRAB_START = 0.7;
-  const float MIN_GRAB_CONTINUE = 0.6;
-  const float MIN_PINCH_START = 0.75;
-  const float MIN_PINCH_CONTINUE = 0.65;
+  const float MIN_GRAB_START = 0.7f;
+  const float MIN_GRAB_CONTINUE = 0.6f;
+  const float MIN_PINCH_START = 0.75f;
+  const float MIN_PINCH_CONTINUE = 0.65f;
   
   float getCustomPinchStrength(const Leap::Hand& hand) const;
   float calcHandPinchVelocity(const Leap::Finger& index, const Leap::Finger& thumb) const;
