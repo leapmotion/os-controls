2852c1e03dfc17501afaf79e86047ba248cf5f5a
Jonathan Marsden <jmarsden@leapmotion.com>
2014-09-09 14:16:33 -0700

    Handle overlay windows from other processes

--- src/osinterface/OSWindowMac.mm
+++ src/osinterface/OSWindowMac.mm
@@ -10,7 +10,11 @@
 #include <cassert>
 
 OSWindowMac::OSWindowMac(NSDictionary* info) :
-  m_windowID([[info objectForKey:(id)kCGWindowNumber] unsignedIntValue]), m_info([info retain]), m_mark(0)
+  m_windowID([[info objectForKey:(id)kCGWindowNumber] unsignedIntValue]),
+  m_overlayWindowID(0),
+  m_overlayOffset(NSZeroPoint),
+  m_info([info retain]),
+  m_mark(0)
 {
 }
 
@@ -23,6 +27,21 @@ void OSWindowMac::UpdateInfo(NSDictionary* info) {
   assert([[info objectForKey:(id)kCGWindowNumber] unsignedIntValue] == m_windowID);
   [m_info release];
   m_info = [info retain];
+}
+
+bool OSWindowMac::SetOverlayWindow(CGWindowID overlayWindowID, const CGPoint& overlayOffset) {
+  if (m_overlayWindowID == overlayWindowID) {
+    if (m_overlayWindowID == 0 ||
+       (m_overlayOffset.x == overlayOffset.x &&
+        m_overlayOffset.y == overlayOffset.y)) {
+      return false;
+    }
+  } else {
+    m_overlayWindowID = overlayWindowID;
+  }
+  m_overlayOffset.x = overlayOffset.x;
+  m_overlayOffset.y = overlayOffset.y;
+  return true;
 }
 
 bool OSWindowMac::IsValid(void) {
@@ -45,49 +64,95 @@ std::shared_ptr<OSApp> OSWindowMac::GetOwnerApp(void) {
 std::shared_ptr<ImagePrimitive> OSWindowMac::GetWindowTexture(std::shared_ptr<ImagePrimitive> img)  {
   CGImageRef imageRef = CGWindowListCreateImage(CGRectNull, kCGWindowListOptionIncludingWindow,
                                                 m_windowID, kCGWindowImageNominalResolution);
-  if (imageRef) {
-    CFDataRef dataRef = CGDataProviderCopyData(CGImageGetDataProvider(imageRef));
-    if (dataRef) {
-      const uint8_t* dstBytes = CFDataGetBytePtr(dataRef);
-      const size_t bytesPerRow = CGImageGetBytesPerRow(imageRef);
-      // const size_t width = CGImageGetWidth(imageRef);
-      // For now, adjust the width to be that of the stride -- FIXME
-      assert(bytesPerRow % 4 == 0);
-      const size_t width = bytesPerRow/4;
-      const size_t height = CGImageGetHeight(imageRef);
-      const size_t totalBytes = bytesPerRow*height;
-
-      // See if the texture underlying image was resized or not. If so, we need to create a new texture
-      std::shared_ptr<GLTexture2> texture = img->Texture();
-      if (texture) {
-        const auto& params = texture->Params();
-        if (params.Height() != height || params.Width() != width) {
-          texture.reset();
-        }
-      }
-      if (texture) {
-        texture->UpdateTexture(dstBytes); // Very dangerous function interface!
-      } else {
-        GLTexture2Params params{static_cast<GLsizei>(width), static_cast<GLsizei>(height)};
-        params.SetTarget(GL_TEXTURE_2D);
-        params.SetInternalFormat(GL_RGBA8);
-        params.SetTexParameteri(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        params.SetTexParameteri(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        params.SetTexParameteri(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        params.SetTexParameteri(GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        GLTexture2PixelDataReference pixelData{GL_BGRA, GL_UNSIGNED_BYTE, dstBytes, totalBytes};
-
-        texture = std::make_shared<GLTexture2>(params, pixelData);
-        img->SetTexture(texture);
-        img->SetScaleBasedOnTextureSize();
-      }
-      texture->Bind();
-      glGenerateMipmap(GL_TEXTURE_2D);
-      texture->Unbind();
-      CFRelease(dataRef);
-    }
-    CFRelease(imageRef);
+  if (!imageRef) {
+    return img;
   }
+  GLenum format = GL_BGRA;
+
+  // If this window has an overlay window, apply the overlay to our image
+  if (m_overlayWindowID) {
+    CGImageRef overlayImageRef = CGWindowListCreateImage(CGRectNull, kCGWindowListOptionIncludingWindow,
+                                                         m_overlayWindowID, kCGWindowImageNominalResolution);
+    if (overlayImageRef) {
+      CGColorSpaceRef rgb = CGColorSpaceCreateDeviceRGB();
+      // Determine actual window size
+      CGRect bounds = NSZeroRect;
+      @autoreleasepool {
+        NSDictionary* windowBounds = [m_info objectForKey:(id)kCGWindowBounds];
+        CGRectMakeWithDictionaryRepresentation(reinterpret_cast<CFDictionaryRef>(windowBounds), &bounds);
+      }
+      const CGRect originalRect{0.0f, 0.0f,
+                                static_cast<CGFloat>(CGImageGetWidth(imageRef)),
+                                static_cast<CGFloat>(CGImageGetHeight(imageRef))};
+      // Adjust the overlay offset based on window decorations
+      const CGFloat dx = (originalRect.size.width - bounds.size.width)*0.5f;
+      const CGFloat dy = (originalRect.size.height - bounds.size.height)*0.71f; // Approx. 71% is bottom decoration
+      // Adjust the offset due to the draw-origin being the bottom-left corner, but the overlay-origin is top-left
+      const CGRect overlayRect{m_overlayOffset.x + dx,
+                               bounds.size.height - CGImageGetHeight(overlayImageRef) - m_overlayOffset.y + dy,
+                               static_cast<CGFloat>(CGImageGetWidth(overlayImageRef)),
+                               static_cast<CGFloat>(CGImageGetHeight(overlayImageRef))};
+      CGContextRef contextRef =
+          CGBitmapContextCreate(nullptr, CGImageGetWidth(imageRef), CGImageGetHeight(imageRef),
+                                8, 0, rgb, kCGImageAlphaPremultipliedLast | kCGBitmapByteOrderDefault);
+      CGContextDrawImage(contextRef, originalRect, imageRef);
+      CGContextDrawImage(contextRef, overlayRect, overlayImageRef);
+      CGImageRef compositeImageRef = CGBitmapContextCreateImage(contextRef);
+      CGContextRelease(contextRef);
+      CGColorSpaceRelease(rgb);
+      CFRelease(overlayImageRef);
+
+      if (compositeImageRef) {
+        CFRelease(imageRef);
+        imageRef = compositeImageRef;
+        format = GL_RGBA;
+      }
+    }
+  }
+
+  CFDataRef dataRef = CGDataProviderCopyData(CGImageGetDataProvider(imageRef));
+  if (dataRef) {
+    const uint8_t* dstBytes = CFDataGetBytePtr(dataRef);
+    const size_t bytesPerRow = CGImageGetBytesPerRow(imageRef);
+    // const size_t width = CGImageGetWidth(imageRef);
+    // For now, adjust the width to be that of the stride -- FIXME
+    assert(bytesPerRow % 4 == 0);
+    const size_t width = bytesPerRow/4;
+    const size_t height = CGImageGetHeight(imageRef);
+    const size_t totalBytes = bytesPerRow*height;
+
+    // See if the texture underlying image was resized or not. If so, we need to create a new texture
+    std::shared_ptr<GLTexture2> texture = img->Texture();
+    if (texture) {
+      const auto& params = texture->Params();
+      if (params.Height() != height || params.Width() != width) {
+        texture.reset();
+      }
+    }
+    if (texture) {
+      texture->UpdateTexture(dstBytes); // Very dangerous function interface!
+    } else {
+      GLTexture2Params params{static_cast<GLsizei>(width), static_cast<GLsizei>(height)};
+      params.SetTarget(GL_TEXTURE_2D);
+      params.SetInternalFormat(GL_RGBA8);
+      params.SetTexParameteri(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+      params.SetTexParameteri(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+      params.SetTexParameteri(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      params.SetTexParameteri(GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+      GLTexture2PixelDataReference pixelData{format, GL_UNSIGNED_BYTE, dstBytes, totalBytes};
+
+      texture = std::make_shared<GLTexture2>(params, pixelData);
+      img->SetTexture(texture);
+      img->SetScaleBasedOnTextureSize();
+    }
+    texture->Bind();
+    glGenerateMipmap(GL_TEXTURE_2D);
+    texture->Unbind();
+    CFRelease(dataRef);
+  }
+
+  CFRelease(imageRef);
+
   return img;
 }
 
