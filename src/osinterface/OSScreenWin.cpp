8bde5d8f92950d71cb1796dc31fadf193993652b
Jonathan Marsden <jmarsden@leapmotion.com>
2014-09-10 16:36:38 -0700

    Obtain background image for default monitor

--- src/osinterface/OSScreenWin.cpp
+++ src/osinterface/OSScreenWin.cpp
@@ -1,9 +1,13 @@
 // Copyright (c) 2010 - 2014 Leap Motion. All rights reserved. Proprietary and confidential.
 #include "stdafx.h"
 #include "OSScreen.h"
-#include "GLTexture2.h"
-#include "utility/SamplePrimitives.h"
+#include <Primitives.h>
+#include <GLTexture2.h>
 #include <ShellScalingApi.h>
+#include <SFML/Graphics/Image.hpp>
+
+#include <fstream>
+#include <cmath>
 
 HMODULE hshcore = LoadLibrary("shcore");
 auto g_GetDpiForMonitor = (decltype(GetDpiForMonitor)*) GetProcAddress(hshcore, "GetDpiForMonitor");
@@ -28,7 +32,7 @@ void OSScreen::Update()
     UINT dpiX;
     UINT dpiY;
     g_GetDpiForMonitor(m_screenID, MDT_RAW_DPI, &dpiX, &dpiY);
-    m_pixelsPerInch = sqrt(dpiX * dpiX + dpiY * dpiY);
+    m_pixelsPerInch = static_cast<float>(std::sqrt(dpiX * dpiX + dpiY * dpiY));
   }
   else
     // Default on Windows is 96/in
@@ -37,6 +41,60 @@ void OSScreen::Update()
 
 std::shared_ptr<ImagePrimitive> OSScreen::GetBackgroundTexture(std::shared_ptr<ImagePrimitive> img) const
 {
-  auto sz = Size();
-  return MakePatternedTexture((size_t) sz.width, (size_t) sz.height);
+  WCHAR pvParam[1024] = {0};
+  if (!SystemParametersInfoW(SPI_GETDESKWALLPAPER, 1024, pvParam, 0)) {
+    return img;
+  }
+  std::wstring pathString(pvParam);
+  if (pathString.empty()) {
+    return img;
+  }
+  std::ifstream ifs(pathString.c_str(), std::ios::in | std::ios::binary);
+  std::string str((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+  ifs.close();
+  if (str.empty()) {
+    return img;
+  }
+  sf::Image image;
+  if (!image.loadFromMemory(str.data(), str.size())) {
+    return img;
+  }
+  str.clear();
+  const uint8_t* dstBytes = static_cast<const uint8_t*>(image.getPixelsPtr());
+  if (!dstBytes) {
+    return img;
+  }
+  const auto size = image.getSize();
+  const size_t width = static_cast<size_t>(size.x);
+  const size_t height = static_cast<size_t>(size.y);
+  const size_t totalBytes = width*height*4;
+
+  std::shared_ptr<GLTexture2> texture = img->Texture();
+  if (texture) {
+    const auto& params = texture->Params();
+    if (params.Height() != height || params.Width() != width) {
+      texture.reset();
+    }
+  }
+  if (texture) {
+    texture->UpdateTexture(dstBytes); // Very dangerous function interface!
+  } else {
+    GLTexture2Params params{static_cast<GLsizei>(width), static_cast<GLsizei>(height)};
+    params.SetTarget(GL_TEXTURE_2D);
+    params.SetInternalFormat(GL_RGBA8);
+    params.SetTexParameteri(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    params.SetTexParameteri(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    params.SetTexParameteri(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    params.SetTexParameteri(GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    GLTexture2PixelDataReference pixelData{GL_RGBA, GL_UNSIGNED_BYTE, dstBytes, totalBytes};
+
+    texture = std::make_shared<GLTexture2>(params, pixelData);
+    img->SetTexture(texture);
+    img->SetScaleBasedOnTextureSize();
+  }
+  texture->Bind();
+  glGenerateMipmap(GL_TEXTURE_2D);
+  texture->Unbind();
+
+  return img;
 }
