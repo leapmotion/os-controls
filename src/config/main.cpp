71b3b6954d36e9de1976aa58e6fad0c9e1cd67ba
Walter Gray <wgray@leapmotion.com>
2014-09-10 21:04:48 -0700

    added PlatformInitializer to the config app to properly set the working directory.

--- src/config/main.cpp
+++ src/config/main.cpp
@@ -1,8 +1,11 @@
 #include "configwindow.h"
 #include <QApplication>
+#include "utility/PlatformInitializer.h"
 
 int main(int argc, char *argv[])
 {
+    PlatformInitializer workingDirectoryChanger;
+
     QApplication a(argc, argv);
     ConfigWindow w;
     w.show();
