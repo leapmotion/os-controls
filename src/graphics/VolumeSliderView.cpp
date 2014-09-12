3b05f9b60c9f7642a0ea56cb31d41d886ce30171
Daniel Plemmons <dpx109@gmail.com>
2014-09-11 18:31:13 -0700

    Initial Implementation of VolumeSliderView

--- src/graphics/VolumeSliderView.cpp
+++ src/graphics/VolumeSliderView.cpp
@@ -1,2 +1,113 @@
 #include "stdafx.h"
 #include "VolumeSliderView.h"
+#include "RenderFrame.h"
+
+VolumeSliderView::VolumeSliderView() :
+  m_width(100.0f),
+  m_height(20.0f),
+  m_sliderActivePart(new RectanglePrim()),
+  m_sliderInactivePart(new RectanglePrim()),
+  m_sliderNotchBodyActive(new SVGPrimitive()),
+  m_sliderNotchBodyInactive(new SVGPrimitive())
+{
+  // Initialize smoothed values
+  m_volumeLevel.SetInitialValue(0.0f);
+  m_volumeLevel.SetSmoothStrength(0.5f);
+  
+  m_activationAmount.SetInitialValue(0.0f);
+  m_activationAmount.SetSmoothStrength(0.8f);
+  
+  // Setup SVGs
+  Resource<TextFile> volumeNotchActiveFile("volume-notch-active.svg");
+  Resource<TextFile> volumeNotchInactiveFile("volume-notch-inactive.svg");
+  
+  // Assign SVG data to proper primitives.
+  m_sliderNotchBodyActive->Set(volumeNotchActiveFile->Contents());
+  m_sliderNotchBodyInactive->Set(volumeNotchInactiveFile->Contents());
+  
+  // Calulate the offsets to the svg primitive centers.
+  m_sliderNotchOffset = m_sliderNotchBodyActive->Origin() - (m_sliderNotchBodyActive->Size()/2.0);
+  
+  //Setup Rectangle Bars
+  m_sliderActivePart->SetSize(Vector2(0.0f, m_height));
+  m_sliderInactivePart->SetSize(Vector2(0.0f, m_height));
+  
+  //Setup bar materials
+  m_sliderActivePart->Material().SetDiffuseLightColor(ACTIVE_PART_COLOR);
+  m_sliderActivePart->Material().SetAmbientLightColor(ACTIVE_PART_COLOR);
+  m_sliderActivePart->Material().SetAmbientLightingProportion(1.0f);
+  
+  m_sliderInactivePart->Material().SetDiffuseLightColor(INACTIVE_PART_COLOR);
+  m_sliderInactivePart->Material().SetAmbientLightColor(INACTIVE_PART_COLOR);
+  m_sliderInactivePart->Material().SetAmbientLightingProportion(1.0f);
+}
+
+void VolumeSliderView::Activate() {
+  m_activationAmount.SetGoal(1.0f);
+}
+
+void VolumeSliderView::Deactivate() {
+  m_activationAmount.SetGoal(0.0f);
+}
+
+void VolumeSliderView::SetVolumeLevel(float newVolumeLevel) {
+  newVolumeLevel = std::min(1.0f, std::max(0.0f, newVolumeLevel)); // Clamp incoming volume level
+  m_volumeLevel.SetGoal(newVolumeLevel);
+}
+
+void VolumeSliderView::NudgeVolumeLevel(float dLevel) {
+  float newVolumeLevel = m_volumeLevel + dLevel;
+  newVolumeLevel = std::min(1.0f, std::max(0.0f, newVolumeLevel)); // Clamp incoming volume level
+  m_volumeLevel.SetGoal(newVolumeLevel);
+}
+
+void VolumeSliderView::SetWidth(float newWidth) {
+  newWidth = std::max(0.0f, newWidth);
+  m_width = newWidth;
+}
+
+void VolumeSliderView::SetHeight(float newHeight) {
+  newHeight = std::max(0.0f, newHeight);
+  m_width = newHeight;
+}
+
+float VolumeSliderView::GetNotchOffset() const {
+  Vector3 notchPosition = m_sliderNotchBodyActive->Translation();
+  return notchPosition.x();
+}
+
+void VolumeSliderView::AnimationUpdate(const RenderFrame &frame) {
+  //Update Smoothed Values
+  m_volumeLevel.Update(frame.deltaT.count());
+  m_activationAmount.Update(frame.deltaT.count());
+  
+  //Calculate bar positions
+  float meterLeftEdge = m_width / 2.0f;
+  float activeWidth = m_width * m_volumeLevel;
+  float inactiveWidth = m_width * (1 - m_volumeLevel);
+  Vector2 activePosition(meterLeftEdge + activeWidth/2.0f, 0.0f);
+  Vector2 inactivePosition(meterLeftEdge + activeWidth + inactiveWidth / 2.0f, 0.0f);
+  
+  //Set Bar Positions
+  m_sliderActivePart->SetSize(Vector2(activeWidth, m_height));
+  m_sliderInactivePart->SetSize(Vector2(inactiveWidth, m_height));
+  m_sliderActivePart->Translation() = Vector3(activePosition.x(), activePosition.y(), 0.0f);
+  m_sliderActivePart->Translation() = Vector3(activePosition.x(), activePosition.y(), 0.0f);
+  
+  //Calculate notch position
+  Vector2 sliderNotchBodyPosition(meterLeftEdge + m_sliderNotchOffset.x() + m_width * m_volumeLevel, m_sliderNotchOffset.y());
+  
+  //Set Active Notch Opacity
+  m_sliderNotchBodyActive->LocalProperties().AlphaMask() = m_activationAmount;
+  
+}
+
+void VolumeSliderView::Render(const RenderFrame &frame) const {
+  // Draw bar
+  PrimitiveBase::DrawSceneGraph(*m_sliderActivePart, frame.renderState);
+  PrimitiveBase::DrawSceneGraph(*m_sliderInactivePart, frame.renderState);
+  
+  //Draw notch body.
+  PrimitiveBase::DrawSceneGraph(*m_sliderNotchBodyInactive, frame.renderState);
+  PrimitiveBase::DrawSceneGraph(*m_sliderNotchBodyActive, frame.renderState);
+}
--- src/graphics/VolumeSliderView.h
+++ src/graphics/VolumeSliderView.h
@@ -1,9 +1,60 @@
 #pragma once
 #include "RenderEngine.h"
-#include <Primitives.h>
+#include "GLShader.h"
+#include "GLShaderLoader.h"
+#include "GLTexture2.h"
+#include "TextFile.h"
+#include "Resource.h"
+#include "Primitives.h"
+#include <SVGPrimitive.h>
+#include <memory>
+#include <Animation.h>
 
 class VolumeSliderView :
   public Renderable
 {
+public:
+
+  VolumeSliderView();
+  virtual~VolumeSliderView() {};
   
+  void Update();
+  
+  // Have the menu display its active or inactive state
+  void Activate();
+  void Deactivate();
+  
+  // Set the new goal value for the view
+  void SetVolumeLevel(float newVolumeLevel);
+  void NudgeVolumeLevel(float dLevel);
+  
+  // Set the width and height of the menu
+  void SetWidth(float newWidth);
+  void SetHeight(float newHeight);
+  
+  // Get the width and height of the view
+  float Width() const { return m_width; }
+  float Height() const { return m_height; }
+  float GetNotchOffset() const;
+  
+  // Implement Renderable
+  void AnimationUpdate(const RenderFrame& frame) override;
+  void Render(const RenderFrame& frame) const override;
+  
+private:
+  const Color ACTIVE_PART_COLOR = Color(0.4f, 0.425f, 0.45f, 0.75f);
+  const Color INACTIVE_PART_COLOR = Color(0.505f, 0.831f, 0.114f, 0.95f);
+  
+  float m_width;
+  float m_height;
+  
+  std::shared_ptr<RectanglePrim> m_sliderActivePart;
+  std::shared_ptr<RectanglePrim> m_sliderInactivePart;
+  std::shared_ptr<SVGPrimitive> m_sliderNotchBodyActive;
+  std::shared_ptr<SVGPrimitive> m_sliderNotchBodyInactive;
+  
+  Vector2 m_sliderNotchOffset;
+  
+  Smoothed<float> m_volumeLevel;
+  Smoothed<float> m_activationAmount;
 };
