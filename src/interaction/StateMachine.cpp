0d40e5f81f663bf9bea0e268e2ab79e3e700dcd5
Walter Gray <wgray@leapmotion.com>
2014-09-12 17:42:10 -0700

    fixed deltaScroll being uninitialized and causing scroll failures on PC

--- src/interaction/StateMachine.cpp
+++ src/interaction/StateMachine.cpp
@@ -173,7 +173,8 @@ void StateMachine::doHandScroll(const Scroll& scroll, const HandLocation& handLo
 }
 
 void StateMachine::doPinchScroll(const Scroll& scroll, const HandLocation& handLocation, const HandPinch& pinch,ScrollState& scrollState) {
-  Vector2 deltaScroll;
+  Vector2 deltaScroll = Vector2::Zero();
+
   switch (m_scrollState) {
   case ScrollState::ACTIVE:
     if (!pinch.isPinching) {
