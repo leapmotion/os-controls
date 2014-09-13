41422d2ffb9d655e6a2618928cbd58babae05ddb
Daniel Plemmons <dpx109@gmail.com>
2014-09-12 21:48:59 -0700

    Initial implementation of expose pusher bar.

--- src/expose/ExposeActivationStateMachine.cpp
+++ src/expose/ExposeActivationStateMachine.cpp
@@ -4,16 +4,17 @@
 
 #include "RenderState.h"
 
-const double startAngle = 5 * M_PI/4.0;
-const double endAngle = startAngle + 2 * (M_PI/4.0);
-const int numItems = 1;
-const Color UNSELECTED_COLOR(0.4f, 0.425f, 0.45f, 0.75f);
-const Color SELECTED_COLOR(0.505f, 0.831f, 0.114f, 0.95f);
-const Color GOAL_COLOR(0.505f, 0.831f, 0.114f, 1.0f);
-
 ExposeActivationStateMachine::ExposeActivationStateMachine() :
-m_state(State::INACTIVE)
+  m_state(State::INACTIVE),
+  m_goalStrip(new RectanglePrim()),
+  m_pusherBar(new RectanglePrim()),
+  m_armed(false)
 {
+  m_goalBottomY.SetInitialValue(0.0f);
+  m_pusherBottomY.SetInitialValue(0.0f);
+  
+  m_goalBottomY.SetSmoothStrength(0.7f);
+  m_pusherBottomY.SetSmoothStrength(0.7f);
 }
 
 
@@ -33,18 +34,23 @@ void ExposeActivationStateMachine::AutoFilter(OSCState appState, const HandData&
   switch( m_state )
   {
     case State::INACTIVE:
+      //Transition to ACTIVE
       if(appState == OSCState::EXPOSE_ACTIVATOR_FOCUSED) {
+        m_goalBottomY.SetGoal(GOAL_BOTTOM_Y);
+        m_pusherBottomY.SetGoal(PUSHER_BOTTOM_Y);
         m_state = State::ACTIVE;
       }
       break;
     case State::ACTIVE:
+      //Transition to INACTIVE
       if(appState != OSCState::EXPOSE_ACTIVATOR_FOCUSED) {
-        m_state = State::INACTIVE;
+        transitionToInactive();
       }
       break;
     case State::COMPLETE:
-      if(appState != OSCState::EXPOSE_ACTIVATOR_FOCUSED) {
-        m_state = State::INACTIVE;
+      //TRANSITION TO INACTIVE
+      if(appState != OSCState::EXPOSE_ACTIVATOR_FOCUSED && appState != OSCState::EXPOSE_FOCUSED) {
+        transitionToInactive();
       }
       break;
     case State::FINAL:
@@ -58,6 +64,20 @@ void ExposeActivationStateMachine::AutoFilter(OSCState appState, const HandData&
       break;
     case State::ACTIVE:
     {
+      float yDiff = PUSHER_BOTTOM_Y - handData.locationData.y;
+      float diffPercent = yDiff / PUSHER_BOTTOM_Y;
+      diffPercent = std::min(1.0f, std::max(0.0f, diffPercent));
+      Color blended = blendColor(UNSELECTED_COLOR, SELECTED_COLOR, diffPercent);
+      m_pusherBottomY.SetInitialValue( std::min(handData.locationData.y, PUSHER_BOTTOM_Y) );
+      m_pusherBottomY.SetGoal( std::min(handData.locationData.y, PUSHER_BOTTOM_Y) );
+      m_pusherBar->Material().SetAmbientLightColor(blended);
+      m_pusherBar->Material().SetDiffuseLightColor(blended);
+      m_pusherBar->Material().SetAmbientLightColor(blended);
+      
+      if( diffPercent >= 1) {
+        resolveSelection();
+        transitionToInactive();
+      }
     }
     case State::FINAL:
     default:
@@ -65,16 +85,48 @@ void ExposeActivationStateMachine::AutoFilter(OSCState appState, const HandData&
   }
 }
 
+Color ExposeActivationStateMachine::blendColor(Color c1, Color c2, float amnt) {
+  Color retColor = Color();
+  
+  amnt = std::min(1.0f, std::max(0.0f, amnt));
+  
+  retColor.R() = ( amnt * c1.R()  ) + ( (1-amnt) * c2.R() );
+  retColor.G() = ( amnt * c1.G()  ) + ( (1-amnt) * c2.G() );
+  retColor.B() = ( amnt * c1.B()  ) + ( (1-amnt) * c2.B() );
+  retColor.A() = ( amnt * c1.A()  ) + ( (1-amnt) * c2.A() );
+  
+  return retColor;
+}
+
+void ExposeActivationStateMachine::transitionToInactive() {
+  m_goalBottomY.SetGoal(0.0f);
+  m_pusherBottomY.SetGoal(0.0f);
+  m_state = State::INACTIVE;
+}
+
 void ExposeActivationStateMachine::resolveSelection() {
   m_stateChangeEvent(&OSCStateChangeEvent::RequestTransition)(OSCState::EXPOSE_FOCUSED);
 }
 
 void ExposeActivationStateMachine::AnimationUpdate(const RenderFrame &renderFrame) {
-
+  m_goalBottomY.Update(renderFrame.deltaT.count());
+  m_pusherBottomY.Update(renderFrame.deltaT.count());
+  
+  float barWidth = m_renderWindow->getSize().x;
+  float goalStripY = m_goalBottomY - (GOAL_BOTTOM_Y/2.0f);
+  float pusherStripY = m_pusherBottomY - (PUSHER_BOTTOM_Y/2.0f);
+  
+  m_pusherBar->SetSize(Vector2(barWidth, PUSHER_BOTTOM_Y));
+  m_goalStrip->SetSize(Vector2(barWidth, GOAL_BOTTOM_Y));
+  
+  m_pusherBar->Translation() = Vector3(0.0f, pusherStripY, 0.0f);
+  m_goalStrip->Translation() = Vector3(0.0f, goalStripY, 0.0f);
 }
 
 void ExposeActivationStateMachine::Render(const RenderFrame &renderFrame) const  {
   if (m_state == State::ACTIVE || m_state == State::COMPLETE) {
+    PrimitiveBase::DrawSceneGraph(*m_pusherBar, renderFrame.renderState);
+    PrimitiveBase::DrawSceneGraph(*m_goalStrip, renderFrame.renderState);
   }
 }
 
--- src/expose/ExposeActivationStateMachine.h
+++ src/expose/ExposeActivationStateMachine.h
@@ -4,6 +4,8 @@
 #include "interaction/HandDataCombiner.h"
 #include "uievents/OSCDomain.h"
 
+#include "Primitives.h"
+#include "Animation.h"
 #include <autowiring/Autowiring.h>
 
 class ExposeActivationStateMachine  :
@@ -20,8 +22,17 @@ public:
   
   void AnimationUpdate(const RenderFrame& renderFrame) override;
   void Render(const RenderFrame& renderFrame) const override;
+  
 private:
+  const Color UNSELECTED_COLOR = Color(0.4f, 0.425f, 0.45f, 0.75f);
+  const Color SELECTED_COLOR = Color(0.505f, 0.831f, 0.114f, 0.95f);
+  const Color GOAL_COLOR = Color(0.505f, 0.831f, 0.114f, 1.0f);
+  const float GOAL_BOTTOM_Y = 30.0f;
+  const float PUSHER_BOTTOM_Y = 150.0f;
+  
+  void transitionToInactive();
   void resolveSelection();
+  Color blendColor(Color c1, Color c2, float amnt);
   
   enum class State {
     
@@ -43,6 +54,12 @@ private:
   std::shared_ptr<RectanglePrim> m_goalStrip;
   std::shared_ptr<RectanglePrim> m_pusherBar;
   
+  bool m_armed;
+  
+  Smoothed<float> m_goalBottomY;
+  Smoothed<float> m_pusherBottomY;
+  
+  Autowired<sf::RenderWindow> m_renderWindow; // Grabbing this for layout purposes.
   Autowired<RenderEngine> m_rootNode;
   AutoFired<OSCStateChangeEvent> m_stateChangeEvent;
 };
