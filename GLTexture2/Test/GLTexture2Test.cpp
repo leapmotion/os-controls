d1fb3456d3b0014c3a5d9b5f859930beea8c5c73
Victor 'Yes, The Victor' Dods <vdods@leapmotion.com>
2014-09-14 13:16:23 -0700

    Fixed the unit test for procedurally generated texture.

--- GLTexture2/Test/GLTexture2Test.cpp
+++ GLTexture2/Test/GLTexture2Test.cpp
@@ -1,10 +1,8 @@
 #include "GLTestFramework.h"
 #include "GLTexture2.h"
-#include "GLTexture2Loader.h"
 #include <gtest/gtest.h>
 #include <iostream>
 #include <memory>
-#include "Resource.h"
 #include <vector>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -90,76 +88,67 @@ class GLTexture2VisibleTest : public GLTestFramework_Visible { };
 TEST_F(GLTexture2VisibleTest, ProcedurallyGenerated) {
   std::shared_ptr<GLTexture2> texture;
   
-  if (true) {
-    // Generate a texture procedurally for use in TexturedFrame.
+  // Generate a texture procedurally.
+  {
     GLsizei width = 4;
     GLsizei height = 4;
     GLTexture2Params params(width, height, GL_RGBA8);
-    params.SetTexParameteri(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    params.SetTexParameteri(GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    params.SetTexParameteri(GL_GENERATE_MIPMAP, GL_TRUE);
+    params.SetTexParameteri(GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    params.SetTexParameteri(GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+    params.SetTexParameteri(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    params.SetTexParameteri(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     
-//     struct RgbPixel { uint8_t r, g, b; };
-    struct RgbPixel { GLfloat r, g, b; };
+    struct RgbPixel { uint8_t r, g, b; };
     
-//     GLTexture2PixelDataStorage<RgbPixel> pixel_data(GL_RGB, GL_UNSIGNED_BYTE, width*height);
-//     std::vector<RgbPixel> &pixels = pixel_data.RawPixels();
-//     std::vector<RgbPixel> pixels(16);
-    RgbPixel pixels[16];
+    GLTexture2PixelDataStorage<RgbPixel> pixel_data(GL_RGB, GL_UNSIGNED_BYTE, width*height);
+    std::vector<RgbPixel> &pixels = pixel_data.RawPixels();
     // Make a simple bilinear gradient in green and blue.
     for (GLsizei v = 0; v < height; ++v) {
       for (GLsizei u = 0; u < width; ++u) {
-//         uint32_t r = 0;
-//         uint32_t g = u*255/(width-1);
-//         uint32_t b = v*255/(height-1);
-//         RgbPixel &pixel = pixels[v*width+u];
-//         pixel = RgbPixel{uint8_t(r), uint8_t(g), uint8_t(b)};
-//         std::cout << "pixel(" << u << ',' << v << ") = (" << uint32_t(pixel.r) << ',' << uint32_t(pixel.g) << ',' << uint32_t(pixel.b) << ")\n";
-        pixels[v*width+u] = RgbPixel{0.0f, u/GLfloat(width-1), v/GLfloat(height-1)};
+        uint32_t r = 0;
+        uint32_t g = u*255/(width-1);
+        uint32_t b = v*255/(height-1);
+        RgbPixel &pixel = pixels[v*width+u];
+        pixel = RgbPixel{uint8_t(r), uint8_t(g), uint8_t(b)};
       }
     }
-//     GLTexture2PixelDataReference pixel_data(GL_RGB, GL_UNSIGNED_BYTE, pixels);
-//     std::cout << "sizeof(pixels) = " << sizeof(pixels) << ", sizeof(RgbPixel) = " << sizeof(RgbPixel) << '\n';
-//     std::cout << "&pixels[0] = " << &pixels[0]
-//               << ", static_cast<const void *>(&pixels[0]) = " << static_cast<const void *>(&pixels[0])
-//               << ", reinterpret_cast<const void *>(&pixels[0]) = " << reinterpret_cast<const void *>(&pixels[0]) << '\n';
-    GLTexture2PixelDataReference pixel_data(GL_RGB, GL_FLOAT, static_cast<const void *>(&pixels[0]), sizeof(pixels));
-      
     texture = std::make_shared<GLTexture2>(params, pixel_data);
-  } else {
-    texture = Resource<GLTexture2>("public_domain_astronomy_1.jpg");
   }
   
-  // render dummy geometry just as a test
-//   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-  glDisable(GL_LIGHTING);
-  glEnable(GL_CULL_FACE);
-  glDisable(GL_DEPTH_TEST);
-  glEnable(GL_TEXTURE_2D);
-    
-  static const GLuint VERTEX_COUNT = 4;
-  const GLfloat param = 0.7f;//0.1f * std::sin(3.0f*m_time) + 0.7f;
-  const GLfloat vertex_array[VERTEX_COUNT*2] = {
-    -param, -param,
-     param, -param,
-     param,  param,
-    -param,  param
-  };
-  static const GLfloat TEXTURE_COORD_ARRAY[VERTEX_COUNT*2] = {
-    0.0f, 0.0f,
-    1.0f, 0.0f,
-    1.0f, 1.0f,
-    0.0f, 1.0f
-  };
-  texture->Bind();
-  glEnableClientState(GL_VERTEX_ARRAY);
-  glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-  glVertexPointer(2, GL_FLOAT, 0, vertex_array);
-  glTexCoordPointer(2, GL_FLOAT, 0, TEXTURE_COORD_ARRAY);
-  glDrawArrays(GL_TRIANGLE_FAN, 0, VERTEX_COUNT);
-  texture->Unbind();
-  
-  m_GLController.EndRender();
-  m_SDLController.EndRender();
+  // Render a textured rectangle.
+  {
+    glDisable(GL_LIGHTING);
+    glEnable(GL_CULL_FACE);
+    glDisable(GL_DEPTH_TEST);
+    glEnable(GL_TEXTURE_2D);
+      
+    static const GLuint VERTEX_COUNT = 4;
+    const GLfloat param = 0.7f;
+    const GLfloat vertex_array[VERTEX_COUNT*2] = {
+      -param, -param,
+       param, -param,
+       param,  param,
+      -param,  param
+    };
+    static const GLfloat TEXTURE_COORD_ARRAY[VERTEX_COUNT*2] = {
+      0.0f, 0.0f,
+      1.0f, 0.0f,
+      1.0f, 1.0f,
+      0.0f, 1.0f
+    };
+    texture->Bind();
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+    glVertexPointer(2, GL_FLOAT, 0, vertex_array);
+    glTexCoordPointer(2, GL_FLOAT, 0, TEXTURE_COORD_ARRAY);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, VERTEX_COUNT);
+    texture->Unbind();
+   
+    // Finish the frame before delaying. 
+    m_GLController.EndRender();
+    m_SDLController.EndRender();
+  }
   
   SDL_Delay(1000); // Delay for 1000 milliseconds.
 }
