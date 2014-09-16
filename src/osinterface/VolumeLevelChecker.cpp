5a5f008b91bf05dc59e29b6bf4f0d57b2a87adb7
Jason Lokerson <cmercenary@gmail.com>
2014-09-16 12:33:47 -0700

    Adding an OnMuteChanged event
    This allows consumers to listen to changes in the mute status, which are distinct from changes in the volume level

--- src/osinterface/VolumeLevelChecker.cpp
+++ src/osinterface/VolumeLevelChecker.cpp
@@ -3,6 +3,13 @@
 #include "AudioVolumeInterface.h"
 #include "uievents/SystemMultimediaEventListener.h"
 
+VolumeLevelChecker::VolumeLevelChecker(void):
+  m_oldMute(true),
+  m_oldVolume(0.0f)
+{
+
+}
+
 void VolumeLevelChecker::Tick(std::chrono::duration<double> deltaT) {
   float volume = m_avi->GetVolume();
   if(m_oldVolume == volume)
@@ -10,4 +17,8 @@ void VolumeLevelChecker::Tick(std::chrono::duration<double> deltaT) {
 
   m_smel(&SystemMultimediaEventListener::OnVolumeChanged)(m_oldVolume, volume);
   m_oldVolume = volume;
+
+  bool muted = m_avi->IsMuted();
+  m_smel(&SystemMultimediaEventListener::OnMuteChanged)(muted);
+  m_oldMute = muted;
 }
