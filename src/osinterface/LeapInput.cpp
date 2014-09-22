77b6f7bfce8da78f6944950dda1f19c1dd952fa5
Jonathan Marsden <jmarsden@leapmotion.com>
2014-09-22 09:47:39 -0700

    Send invalid Leap frame when device disconnects or screen saver is enabled

--- src/osinterface/LeapInput.cpp
+++ src/osinterface/LeapInput.cpp
@@ -1,10 +1,12 @@
 #include "stdafx.h"
 #include "LeapInput.h"
 #include "LeapInputListener.h"
+#include "OSVirtualScreen.h"
 #include "interaction/FrameFragmenter.h"
 
 LeapInput::LeapInput(void):
-  ContextMember("LeapInput")
+  ContextMember("LeapInput"),
+  m_isAcceptingInput(false)
 {
   m_controller->enableGesture(Leap::Gesture::TYPE_CIRCLE);
   m_controller->setPolicyFlags(Leap::Controller::POLICY_BACKGROUND_FRAMES);
@@ -16,7 +18,29 @@ LeapInput::~LeapInput(void)
   m_controller->removeListener(*this);
 }
 
+bool LeapInput::AcceptInput(void) const {
+  // Ignore input when the screen saver is running
+  return m_virtualScreen && !m_virtualScreen->IsScreenSaverActive();
+}
+
+void LeapInput::onDisconnect(const Leap::Controller& controller) {
+  m_isAcceptingInput = false;
+  CurrentContextPusher pshr(this->GetContext());
+  // Send an invalid frame when the device has disconnected
+  m_listener(&LeapInputListener::OnLeapFrame)(Leap::Frame::invalid());
+}
+
 void LeapInput::onFrame(const Leap::Controller& controller) {
+  if (!AcceptInput()) {
+    if (m_isAcceptingInput) {
+      m_isAcceptingInput = false;
+      CurrentContextPusher pshr(this->GetContext());
+      // Send an invalid frame when transitioning from "accepting input" to "not-accepting input"
+      m_listener(&LeapInputListener::OnLeapFrame)(Leap::Frame::invalid());
+    }
+    return;
+  }
+  m_isAcceptingInput = true;
   CurrentContextPusher pshr(this->GetContext());
   m_listener(&LeapInputListener::OnLeapFrame)(controller.frame());
 }
--- src/osinterface/LeapInput.h
+++ src/osinterface/LeapInput.h
@@ -2,6 +2,7 @@
 #include <autowiring/ContextMember.h>
 #include <Leap.h>
 
+class OSVirtualScreen;
 class FrameFragmenter;
 class LeapInputListener;
 
@@ -18,10 +19,16 @@ public:
 
 private:
   AutoRequired<Leap::Controller> m_controller;
+  Autowired<OSVirtualScreen> m_virtualScreen;
+  bool m_isAcceptingInput;
 
   // Event raised when a leap frame comes in
   AutoFired<LeapInputListener> m_listener;
 
+  // Determine whether or not to accepting Leap input
+  bool AcceptInput(void) const;
+
   // Leap::Listener overrides:
+  void onDisconnect(const Leap::Controller& controller) override;
   void onFrame(const Leap::Controller& controller) override;
 };
