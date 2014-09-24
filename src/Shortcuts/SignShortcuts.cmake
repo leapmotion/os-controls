fc5dd9f018044e81bcab0c98b849533a3bb540e8
Walter Gray <wgray@leapmotion.com>
2014-09-24 12:03:32 -0700

    Added signing to the install step.

--- src/Shortcuts/SignShortcuts.cmake
+++ src/Shortcuts/SignShortcuts.cmake
@@ -6,8 +6,14 @@ if(WIN32)
   find_program(ManifestTool mt.exe PATHS "C:\\Program Files (x86)\\Windows Kits")
   execute_process(
     COMMAND mt.exe "-updateresource:${_shortcuts}" -manifest uiAccess.manifest
+    COMMAND signtool.exe sign -a -sm -n "Leap Motion Inc" 
+      -t http://timestamp.verisign.com/scripts/timstamp.dll ${_shortcuts}
     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-    OUTPUT_FILE logout.txt ERROR_FILE logerror.txt
+    OUTPUT_FILE logout.txt ERROR_FILE logerror.txt RESULT_VARIABLE _runresult
   )
+
+if(NOT _runresult EQUAL 0)
+  message(FATAL_ERROR "Signing failed - see logout.txt and logerror.txt for more info.")
+endif()
 
 endif()
