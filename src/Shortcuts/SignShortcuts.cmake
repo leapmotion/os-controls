762c98a3477b2f54a2f7dab0cf175119aea07008
Walter Gray <wgray@leapmotion.com>
2014-09-24 18:10:12 -0700

    Fixed the mt.exe command and the signtool command running in parallel.

--- src/Shortcuts/SignShortcuts.cmake
+++ src/Shortcuts/SignShortcuts.cmake
@@ -1,15 +1,15 @@
 if(WIN32)
 
   set(_shortcuts "${CMAKE_INSTALL_PREFIX}/Shortcuts.exe")
-  message(working_dir=${CMAKE_CURRENT_LIST_DIR})
 
-  find_program(ManifestTool mt.exe PATHS "C:\\Program Files (x86)\\Windows Kits")
   execute_process(
     COMMAND mt.exe "-updateresource:${_shortcuts}" -manifest uiAccess.manifest
+    OUTPUT_FILE manifestlogout.txt ERROR_FILE manifestlogerror.txt RESULT_VARIABLE _runresult
+  )
+  execute_process(
     COMMAND signtool.exe sign -debug -v -a -n "Leap Motion Inc" 
       -t http://timestamp.verisign.com/scripts/timstamp.dll ${_shortcuts}
-    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-    OUTPUT_FILE logout.txt ERROR_FILE logerror.txt RESULT_VARIABLE _runresult
+    OUTPUT_FILE signlogout.txt ERROR_FILE signlogerror.txt RESULT_VARIABLE _runresult
   )
 
 if(NOT _runresult EQUAL 0)
