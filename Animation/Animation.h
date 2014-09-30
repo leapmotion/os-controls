358ade517ecb9a771a92dfaca85b65241296befb
Walter Gray <wgray@leapmotion.com>
2014-09-29 20:12:51 -0700

    Renamed SetGoalAndValue to SetImmediate for better parity with Animated<T>

--- Animation/Animation.h
+++ Animation/Animation.h
@@ -132,7 +132,7 @@ public:
   Smoothed() : Smoothed(0) { }
   Smoothed(const T& initialValue, float smoothStrength = 0.8f, float targetFramerate = 100.0f) :
     m_TargetFramerate(targetFramerate), m_SmoothStrength(smoothStrength) {
-    SetGoalAndValue(initialValue);
+    SetImmediate(initialValue);
   }
 
   // const getters
@@ -142,14 +142,14 @@ public:
 
   // setters to control animation
   void SetGoal(const T& goal) { m_Goal = goal; }
-  void SetSmoothStrength(float smooth) { m_SmoothStrength = smooth; }
   // sets the goal and value to the same number
-  void SetGoalAndValue(const T& value) {
+  void SetImmediate(const T& value) {
     m_Goal = value;
     for (int i = 0; i<NUM_ITERATIONS; i++) {
       m_Values[i] = value;
     }
   }
+  void SetSmoothStrength(float smooth) { m_SmoothStrength = smooth; }
 
   DEPRECATED(
       void SetInitialValue(const T& value) {
