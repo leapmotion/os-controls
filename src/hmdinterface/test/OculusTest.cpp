8c0a5e8e88710c2e456f6fc354f96d7a9cc2efa6
Walter Gray <wgray@leapmotion.com>
2014-10-17 12:36:48 -0700

    Initial add of hmdinterfacetest project

---
+++ src/hmdinterface/test/OculusTest.cpp
@@ -0,0 +1,19 @@
+// Copyright (C) 2012-2014 Leap Motion, Inc. All rights reserved.
+#include "stdafx.h"
+#include "hmdinterface/OculusRift/RiftContext.h"
+#include "hmdinterface/OculusRift/RiftDevice.h"
+#include "osinterface/RenderWindow.h"
+
+class OculusTest :
+public testing::Test
+{};
+
+TEST_F(OculusTest, BasicSquare) {
+  std::shared_ptr<RenderWindow> window1(RenderWindow::New());
+  window1->SetRect({ 300, 300, 640, 480 });
+  window1->SetTransparent(false);
+  window1->SetVisible(true);
+
+
+}
+
---
+++ src/hmdinterface/test/hmdinterfacetest.cpp
@@ -0,0 +1,7 @@
+// Copyright (C) 2012-2014 Leap Motion, Inc. All rights reserved.
+#include <autowiring/gtest-all-guard.h>
+
+int main(int argc, const char* argv[])
+{
+  return autotesting_main(argc, argv);
+}
