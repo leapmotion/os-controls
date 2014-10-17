0400d40c7c1d4037cb23587c55ecf63795863369
Walter Gray <wgray@leapmotion.com>
2014-10-17 16:23:12 -0700

    Fleshed out OculusTest

--- src/hmdinterface/test/OculusTest.cpp
+++ src/hmdinterface/test/OculusTest.cpp
@@ -9,18 +9,30 @@ public testing::Test
 {};
 
 TEST_F(OculusTest, BasicSquare) {
+  glewInit();
+
   std::shared_ptr<RenderWindow> window1(RenderWindow::New());
   window1->SetRect({ 300, 300, 640, 480 });
   window1->SetTransparent(false);
   window1->SetVisible(true);
 
+  auto hmdContext = std::make_shared<OculusRift::Context>();
+  auto hmdDevice = std::make_shared<OculusRift::Device>();
+  hmdContext->Initialize();
 
+  hmdDevice->SetWindow(window1->GetSystemHandle());
+  hmdDevice->Initialize(*hmdContext);
 
+  while (true) {
     hmdDevice->BeginFrame();
     for (int eyeIndex = 0; eyeIndex < 2; eyeIndex++) {
       hmdDevice->BeginRenderingEye(eyeIndex);
       glClearColor(0, 0, 1, 0);
       glClear(GL_COLOR_BUFFER_BIT);
       glFlush();
+      hmdDevice->EndRenderingEye(eyeIndex);
+    }
+    hmdDevice->EndFrame();
+  }
 }
 
