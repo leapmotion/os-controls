b84ddb88d3a0f0c0e31e562082a8eb6ce40fb710
Victor Dods <victor.dods@gmail.com>
2014-10-17 18:16:44 -0700

    Did a bunch more work on the system wipe recognizer, trying to munge the image data into a more manageable form.

--- src/SystemWipeRecognizer/main.cpp
+++ src/SystemWipeRecognizer/main.cpp
@@ -8,6 +8,8 @@ using namespace Leap;
 
 #define FORMAT_VALUE(x) #x << " = " << (x)
 
+
+
 class SampleListener : public Listener {
 public:
     virtual void onConnect(const Controller&);
@@ -27,27 +29,27 @@ void SampleListener::onFrame(const Controller& controller) {
 
     Leap::ImageList images = controller.frame().images();
     // std::cout << "number of images = " << images.count() << '\n';
-    if (images.count() == 0) {
+    if (images.count() < 2) {
         return;
     }
 
-    Leap::Image left_image = images[0];
-    // std::cout << "left image isValid = " << left_image.isValid() << '\n';
-    if (!left_image.isValid()) {
+    Leap::Image image = images[1]; // Use the right image
+    // std::cout << "image isValid = " << image.isValid() << '\n';
+    if (!image.isValid()) {
         return;
     }
 
     // Make a mipmap ofr the brightnesses.
     std::vector<std::vector<float>> brightness_mipmap(1);
-    brightness_mipmap.reserve(std::ceil(1.0f+std::log2(left_image.height())));
+    brightness_mipmap.reserve(std::ceil(1.0f+std::log2(image.height())));
 
     std::vector<float> &brightness = brightness_mipmap.back();
     if (true) {
-        // use the vertical scanline 2/3s of the way across the screen.
-        int x = 2*left_image.width()/3;
-        for (int y = 0; y < left_image.height(); ++y) {
-            int data_index = y*left_image.width() + x;
-            brightness.push_back(left_image.data()[data_index] / 255.0f);
+        // use the vertical scanline a fraction of the way across the screen.
+        int x = 1*image.width()/2;
+        for (int y = 0; y < image.height(); ++y) {
+            int data_index = y*image.width() + x;
+            brightness.push_back(image.data()[data_index] / 255.0f);
         }
     } else {
         size_t sample_count = 20;
@@ -59,10 +61,10 @@ void SampleListener::onFrame(const Controller& controller) {
         {
             float horizontal_slope = 0.0f;
             float vertical_slope = tan(vertical_angle * 180.0f/Leap::PI);
-            Leap::Vector pixel = left_image.warp(Leap::Vector(horizontal_slope, vertical_slope, 0.0f));
+            Leap::Vector pixel = image.warp(Leap::Vector(horizontal_slope, vertical_slope, 0.0f));
             std::cout << FORMAT_VALUE(pixel) << '\n';
-            int data_index = std::floor(pixel.y) * left_image.width() + std::floor(pixel.x);
-            brightness.push_back(left_image.data()[data_index] / 255.0f);
+            int data_index = std::floor(pixel.y) * image.width() + std::floor(pixel.x);
+            brightness.push_back(image.data()[data_index] / 255.0f);
         }
     }
 
@@ -110,16 +112,18 @@ void SampleListener::onFrame(const Controller& controller) {
         //        - 102.001675189 * t^3 + 16.2145167682 * t^2 + 1.70793619066 * t
         //        + 0.43664087912
         static const size_t POLYNOMIAL_ORDER = 6;
-        static const float POLYNOMIAL_COEFFICIENTS[POLYNOMIAL_ORDER+1] = {
-               0.43664087912f, // constant term
-               1.70793619066f, // linear term
-              16.2145167682f,  // quadratic term
-            -102.001675189f,   // etc.
-             219.060195969f,
-            -204.911368221f,
-              69.972969779
-        };
+        // static const float POLYNOMIAL_COEFFICIENTS[POLYNOMIAL_ORDER+1] = {
+        //        0.43664087912f, // constant term
+        //        1.70793619066f, // linear term
+        //       16.2145167682f,  // quadratic term
+        //     -102.001675189f,   // etc.
+        //      219.060195969f,
+        //     -204.911368221f,
+        //       69.972969779
+        // };
         // static const float POLYNOMIAL_COEFFICIENTS[POLYNOMIAL_ORDER+1] = { 0.707890459553f, 4.32712858129f, -25.753521887f, 75.4670191074f, -117.540522017f, 93.9155909561f, -30.3436435936f };
+        static const float POLYNOMIAL_COEFFICIENTS[POLYNOMIAL_ORDER+1] = { 0.410510438512f, 0.827091349285f, 25.0505350679f, -133.066847156f,
+269.812409302f, -243.934782779f, 81.311594216f };
         float power_of_t = 1.0f;
         float retval = 0.0f;
         for (size_t i = 0; i < POLYNOMIAL_ORDER+1; ++i) {
@@ -136,8 +140,8 @@ void SampleListener::onFrame(const Controller& controller) {
         return (b > 0.8f) ? 1 : 0;
     };
 
-    // auto output_format = percent;
-    auto output_format = snap;
+    // auto filtered_value = percent;
+    auto filtered_value = snap;
 
     // std::vector<float> approximated_max_brightness;
     // for (int i = 0; i < m_max_brightness_mipmap[3].size(); ++i) {
@@ -145,33 +149,54 @@ void SampleListener::onFrame(const Controller& controller) {
     //     approximated_max_brightness.push_back(max_brightness_approximation(t));
     // }
 
-    // std::cout << "   ";
+    // std::cout << "    ";
     // for (auto b : m_max_brightness_mipmap[3]) {
-    //     std::cout << std::setw(3) << output_format(b) << ',';
+    //     std::cout << std::setw(3) << percent(b) << ',';
     // }
     // std::cout << '\n';
 
-    // std::cout << "   ";
+    // std::cout << "    ";
     // for (auto b : approximated_max_brightness) {
-    //     std::cout << std::setw(3) << output_format(b) << ',';
+    //     std::cout << std::setw(3) << filtered_value(b) << ',';
     // }
     // std::cout << '\n';
 
-    // std::cout << "   ";
+    // std::cout << "    ";
     // for (auto b : brightness_mipmap[3]) {
-    //     std::cout << std::setw(3) << output_format(b) << ',';
+    //     std::cout << std::setw(3) << percent(b) << ',';
     // }
     // std::cout << '\n';
 
-    std::cout << "   ";
+    float mass = 0.0f;
+    float centroid = 0.0f;
+    size_t sample_count = brightness_mipmap[3].size();
+    for (int i = 0; i < sample_count; ++i) {
+        float t = float(i) / (sample_count - 1);
+        float b = brightness_mipmap[3][i] / max_brightness_approximation(t);
+        float s = snap(b);
+        mass += s;
+        centroid += s*t;
+    }
+    centroid /= mass; // Divide centroid through by mass before adjusting mass by sample_count.
+    mass /= sample_count;
+
     for (int i = 0; i < brightness_mipmap[3].size(); ++i) {
         float t = float(i) / (brightness_mipmap[3].size() - 1);
-        float b = brightness_mipmap[3][i];
-        // std::cout << std::setw(3) << output_format(b/max_brightness_approximation(t)) << ',';
-        std::cout << std::setw(1) << output_format(b/max_brightness_approximation(t));
+        float b = brightness_mipmap[3][i] / max_brightness_approximation(t);
+        // std::cout << std::setw(3) << filtered_value(b) << ',';
+        std::cout << std::setw(1) << filtered_value(b);
     }
+    // std::cout << '\n';
 
-    std::cout << '\n';
+    std::cout << "    ";
+    for (int i = 0; i < brightness_mipmap[3].size(); ++i) {
+        float t = float(i) / (brightness_mipmap[3].size() - 1);
+        float b = brightness_mipmap[3][i] / max_brightness_approximation(t);
+        // std::cout << std::setw(3) << filtered_value(b) << ',';
+        std::cout << (i == int(std::round(centroid*sample_count)) ? 'X' : '.');
+    }
+    std::cout << "    " << std::setw(3) << percent(mass) << '\n';
+
     // std::cout << '\n';
 }
 
