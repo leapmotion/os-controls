cc2de7bb381291fb5f07970b5946b605e63485aa
Walter Gray <wgray@leapmotion.com>
2014-10-20 17:00:40 -0700

    Added another composition test to demonstrate the ability to move composed windows.

--- src/osinterface/test/CompositionEngineTest.cpp
+++ src/osinterface/test/CompositionEngineTest.cpp
@@ -15,6 +15,19 @@ class CompositionEngineTest :
   public testing::Test
 {};
 
+void RenderColoredSquare(float r, float g, float b) {
+  ::glBegin(GL_QUADS);
+  ::glColor3d(r, g, b);
+  ::glVertex3f(-1, -1, -5);
+  ::glColor3d(0, 0, 1);
+  ::glVertex3f(1, -1, -5);
+  ::glColor3d(r, g, b);
+  ::glVertex3f(1, 1, -5);
+  ::glColor3d(0, 0, 1);
+  ::glVertex3f(-1, 1, -5);
+  ::glEnd();
+}
+
 TEST_F(CompositionEngineTest, VerifyBasicVisual) {
   std::shared_ptr<RenderWindow> window1(RenderWindow::New());
   window1->SetRect({0, 0, 640, 480});
@@ -64,21 +77,109 @@ TEST_F(CompositionEngineTest, VerifyBasicVisual) {
 
     ::glPushMatrix();
     ::glRotatef(rotation, 0.0f, 0.0f, 1.0f);
-    ::glBegin(GL_QUADS);
-      ::glColor3d(1,0,0);
-      ::glVertex3f(-1,-1,-5);
-      ::glColor3d(1,1,0);
-      ::glVertex3f(1,-1,-5);
-      ::glColor3d(0,1,0);
-      ::glVertex3f(1,1,-5);
-      ::glColor3d(0,0,1);
-      ::glVertex3f(-1,1,-5);
-    ::glEnd();
+    RenderColoredSquare(1, 0, 0);
     ::glPopMatrix();
 
     rotation += 4.0f;
 
     window2->FlushBuffer();
     window2->SetActive(false);
+  }
+}
+
+TEST_F(CompositionEngineTest, VerifyMovingVisual)
+{
+  std::shared_ptr<RenderWindow> window1(RenderWindow::New());
+  window1->SetRect({ 0, 0, 640, 480 });
+  std::shared_ptr<RenderWindow> window2(RenderWindow::New());
+  window2->SetRect({ 0, 480, 640, 480 });
+  std::shared_ptr<RenderWindow> window3(RenderWindow::New());
+  window3->SetRect({ 640, 0, 640, 480 });
+
+  AutoRequired<CompositionEngine> engine;
+  auto display = engine->CreateDisplay(window1->GetSystemHandle());
+  auto mainView = engine->CreateView();
+  auto viewWindow2 = engine->CreateView();
+  auto viewWindow3 = engine->CreateView();
+
+  viewWindow2->SetContent(window2->GetSystemHandle());
+  viewWindow2->SetScale(0.f, 0.f, 1.0f, 1.0f);
+  viewWindow2->SetOffset(0, 0);
+
+  viewWindow3->SetContent(window3->GetSystemHandle());
+  viewWindow3->SetScale(0.0f, 0.0f, 1.0f, 1.0f);
+  viewWindow3->SetOffset(0, 480);
+
+  mainView->AddChild(viewWindow2);
+  mainView->AddChild(viewWindow3);
+
+  display->SetView(mainView);
+
+  engine->CommitChanges();
+
+  window1->SetTransparent(false);
+  window1->SetVisible(true);
+  window2->SetTransparent(false);
+  window2->SetVSync(true);
+  window2->SetVisible(true);
+  window3->SetVSync(true);
+  window3->SetTransparent(false);
+
+  window2->SetActive(true);
+  const auto windowSize = window2->GetSize();
+  const float ar = static_cast<float>(windowSize.width) / static_cast<float>(windowSize.height);
+  ::glViewport(0, 0, windowSize.width, windowSize.height);
+  ::glMatrixMode(GL_PROJECTION);
+  ::glLoadIdentity();
+  ::glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
+  window2->SetActive(false);
+
+  window3->SetActive(true);
+  ::glViewport(0, 0, windowSize.width, windowSize.height);
+  ::glMatrixMode(GL_PROJECTION);
+  ::glLoadIdentity();
+  ::glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
+  window3->SetActive(false);
+
+  auto start = std::chrono::steady_clock::now();
+  float rotation = 0;
+  float offset = 0;
+  while (std::chrono::steady_clock::now() - start < std::chrono::seconds(5)) {
+    window1->ProcessEvents();
+    window2->ProcessEvents();
+    window3->ProcessEvents();
+
+    window2->SetActive(true);
+
+    ::glClearColor(0, 0, 0, 0);
+    ::glClear(GL_COLOR_BUFFER_BIT);
+
+    ::glPushMatrix();
+    ::glRotatef(rotation, 0.0f, 0.0f, 1.0f);
+    RenderColoredSquare(1, 0, 0);
+    ::glPopMatrix();
+
+    window2->FlushBuffer();
+    window2->SetActive(false);
+
+    window3->SetActive(true);
+
+    ::glClearColor(0, 0, 0, 0);
+    ::glClear(GL_COLOR_BUFFER_BIT);
+
+    ::glPushMatrix();
+    ::glRotatef(rotation, 0.0f, 0.0f, 1.0f);
+    RenderColoredSquare(0, 1, 0);
+    ::glPopMatrix();
+
+    window3->FlushBuffer();
+    window3->SetActive(false);
+
+    rotation += 4.0f;
+    mainView->SetOffset(0,-offset);
+    engine->CommitChanges();
+    offset += 4;
+    offset = std::min(offset, 480.0f);
+
   }
 }
