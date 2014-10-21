2bcfc731e64801319cfc1fbc79a738f54c444145
Victor Dods <victor.dods@gmail.com>
2014-10-21 10:15:51 -0700

    Replaced ad-hoc brightness mipmap code with an encapsulated class with cleanly implemented sampling.

--- src/SystemWipeRecognizer/main.cpp
+++ src/SystemWipeRecognizer/main.cpp
@@ -1,14 +1,62 @@
 #include <cassert>
 #include <iomanip>
 #include <iostream>
-#include <string.h>
+#include <stdexcept>
 #include "Leap.h"
 
 using namespace Leap;
 
 #define FORMAT_VALUE(x) #x << " = " << (x)
 
+template <typename T_>
+class BrightnessMipmap {
+public:
 
+    BrightnessMipmap () { }
+    BrightnessMipmap (const std::vector<T_> &brightness) { Populate(brightness); }
+
+    bool IsInitialized () const { return m_level.size() > 0; }
+    // TODO: linear interpolation in both parameters.
+    const T_ &Sample (size_t at_level, float param) const {
+        if (param < 0.0f || param > 1.0f) {
+            throw std::invalid_argument("param out of range [0,1]");
+        }
+        // This call will throw if at_level is out of range.
+        const std::vector<T_> &level = m_level.at(at_level);
+        // Map the range [0,1] onto [0,level.size()-1].
+        assert(level.size() > 0);
+        return level[static_cast<size_t>(param*(level.size()-1))];
+    }
+
+    void Populate (const std::vector<T_> &brightness) {
+        m_level.clear();
+        // IMPORTANT: Reserve all the space we'll need in m_level so that pointers
+        // to elements of m_level stay valid for the duration of this computation.
+        m_level.reserve(std::ceil(1.0f+std::log2(brightness.size())));
+        m_level.emplace_back(brightness);
+        // Construct all the levels of the mipmap via downsampling.
+        const std::vector<T_> *previous = &m_level.back();
+        while (previous->size()/2 > 0) {
+            m_level.emplace_back();
+            std::vector<T_> &next = m_level.back();
+            Downsample(*previous, next);
+            assert(next.size() > 0);
+            previous = &next;
+        }
+    }
+
+private:
+
+    // Populate out with a half-sized downsampled version of in.
+    static void Downsample (const std::vector<float> &in, std::vector<float> &out) {
+        out.clear();
+        for (size_t y = 0; y+1 < in.size(); y += 2) {
+            out.push_back((in[y]+in[y+1])/2);
+        }
+    };
+
+    std::vector<std::vector<T_>> m_level;
+};
 
 class SampleListener : public Listener {
 public:
@@ -39,68 +87,37 @@ void SampleListener::onFrame(const Controller& controller) {
         return;
     }
 
-    // Make a mipmap ofr the brightnesses.
-    std::vector<std::vector<float>> brightness_mipmap(1);
-    brightness_mipmap.reserve(std::ceil(1.0f+std::log2(image.height())));
+    // // Make a mipmap of the brightnesses.
+    // std::vector<std::vector<float>> brightness_mipmap(1);
+    // brightness_mipmap.reserve(std::ceil(1.0f+std::log2(image.height())));
 
-    std::vector<float> &brightness = brightness_mipmap.back();
-    if (true) {
-        // use the vertical scanline a fraction of the way across the screen.
-        int x = 1*image.width()/2;
-        for (int y = 0; y < image.height(); ++y) {
-            int data_index = y*image.width() + x;
-            brightness.push_back(image.data()[data_index] / 255.0f);
-        }
-    } else {
-        size_t sample_count = 20;
-        float vertical_angle_begin = 20.0f;
-        float vertical_angle_end = -20.0f;
-        for (float vertical_angle = vertical_angle_begin;
-             vertical_angle >= vertical_angle_end;
-             vertical_angle -= std::abs(vertical_angle_end-vertical_angle_begin) / (sample_count-1))
-        {
-            float horizontal_slope = 0.0f;
-            float vertical_slope = tan(vertical_angle * 180.0f/Leap::PI);
-            Leap::Vector pixel = image.warp(Leap::Vector(horizontal_slope, vertical_slope, 0.0f));
-            std::cout << FORMAT_VALUE(pixel) << '\n';
-            int data_index = std::floor(pixel.y) * image.width() + std::floor(pixel.x);
-            brightness.push_back(image.data()[data_index] / 255.0f);
-        }
+    // Sample the image along a single vertical line.
+    std::vector<float> brightness;
+    // use the vertical scanline a fraction of the way across the screen.
+    int x = 1*image.width()/2;
+    for (int y = 0; y < image.height(); ++y) {
+        int data_index = y*image.width() + x;
+        brightness.push_back(image.data()[data_index] / 255.0f);
     }
 
-    auto downsample = [](const std::vector<float> &in, std::vector<float> &out) {
-        out.clear();
-        for (size_t y = 0; y+1 < in.size(); y += 2) {
-            out.push_back(0.5f*(in[y]+in[y+1]));
-        }
-    };
+    // if (m_max_brightness_mipmap.size() != brightness_mipmap.size()) {
+    //     m_max_brightness_mipmap.resize(brightness_mipmap.size());
+    // }
 
-    const std::vector<float> *previous = &brightness_mipmap.back();
-    while (previous->size()/2 > 0) {
-        brightness_mipmap.emplace_back();
-        std::vector<float> &next = brightness_mipmap.back();
-        downsample(*previous, next);
-        previous = &next;
-    }
-
-    if (m_max_brightness_mipmap.size() != brightness_mipmap.size()) {
-        m_max_brightness_mipmap.resize(brightness_mipmap.size());
-    }
-
-    auto take_max_of = [](const std::vector<float> &v, std::vector<float> &dest) {
-        assert(v.size() == dest.size());
-        for (size_t i = 0; i < v.size(); ++i) {
-            if (dest[i] < v[i]) {
-                dest[i] = v[i];
-            }
-        }
-    };
-    for (size_t i = 0; i < brightness_mipmap.size(); ++i) {
-        if (m_max_brightness_mipmap[i].size() != brightness_mipmap[i].size()) {
-            m_max_brightness_mipmap[i].resize(brightness_mipmap[i].size(), 0.0f);
-        }
-        take_max_of(brightness_mipmap[i], m_max_brightness_mipmap[i]);
-    }
+    // auto take_max_of = [](const std::vector<float> &v, std::vector<float> &dest) {
+    //     assert(v.size() == dest.size());
+    //     for (size_t i = 0; i < v.size(); ++i) {
+    //         if (dest[i] < v[i]) {
+    //             dest[i] = v[i];
+    //         }
+    //     }
+    // };
+    // for (size_t i = 0; i < brightness_mipmap.size(); ++i) {
+    //     if (m_max_brightness_mipmap[i].size() != brightness_mipmap[i].size()) {
+    //         m_max_brightness_mipmap[i].resize(brightness_mipmap[i].size(), 0.0f);
+    //     }
+    //     take_max_of(brightness_mipmap[i], m_max_brightness_mipmap[i]);
+    // }
 
     // Use a 6th order polynomial to approximate the maximum brightness curve for the
     // center vertical line for the camera.
@@ -167,34 +184,57 @@ void SampleListener::onFrame(const Controller& controller) {
     // }
     // std::cout << '\n';
 
+    BrightnessMipmap<float> mipmap(brightness);
+
+    // Number of samples uniformly distributed.  TODO: compute which mipmap level to use based on this.
+    static const size_t SAMPLE_COUNT = 40;
     float mass = 0.0f;
     float centroid = 0.0f;
-    size_t sample_count = brightness_mipmap[3].size();
-    for (int i = 0; i < sample_count; ++i) {
-        float t = float(i) / (sample_count - 1);
-        float b = brightness_mipmap[3][i] / max_brightness_approximation(t);
+    for (size_t sample_index = 0; sample_index < SAMPLE_COUNT; ++sample_index) {
+        float t = float(sample_index) / (SAMPLE_COUNT-1);
+        float b = mipmap.Sample(3,t) / max_brightness_approximation(t);
         float s = snap(b);
         mass += s;
         centroid += s*t;
     }
+    // size_t sample_count = mipmap[3].size();
+    // for (int i = 0; i < sample_count; ++i) {
+    //     float t = float(i) / (sample_count - 1);
+    //     float b = mipmap[3][i] / max_brightness_approximation(t);
+    //     float s = snap(b);
+    //     mass += s;
+    //     centroid += s*t;
+    // }
     centroid /= mass; // Divide centroid through by mass before adjusting mass by sample_count.
-    mass /= sample_count;
+    mass /= SAMPLE_COUNT;
 
-    for (int i = 0; i < brightness_mipmap[3].size(); ++i) {
-        float t = float(i) / (brightness_mipmap[3].size() - 1);
-        float b = brightness_mipmap[3][i] / max_brightness_approximation(t);
+    for (size_t sample_index = 0; sample_index < SAMPLE_COUNT; ++sample_index) {
+        float t = float(sample_index) / (SAMPLE_COUNT-1);
+        float b = mipmap.Sample(3,t) / max_brightness_approximation(t);
         // std::cout << std::setw(3) << filtered_value(b) << ',';
-        std::cout << std::setw(1) << filtered_value(b);
+        std::cout << std::setw(1) << snap(b);
     }
+    // for (int i = 0; i < mipmap[3].size(); ++i) {
+    //     float t = float(i) / (mipmap[3].size() - 1);
+    //     float b = mipmap[3][i] / max_brightness_approximation(t);
+    //     // std::cout << std::setw(3) << filtered_value(b) << ',';
+    //     std::cout << std::setw(1) << snap(b);
+    // }
     // std::cout << '\n';
 
     std::cout << "    ";
-    for (int i = 0; i < brightness_mipmap[3].size(); ++i) {
-        float t = float(i) / (brightness_mipmap[3].size() - 1);
-        float b = brightness_mipmap[3][i] / max_brightness_approximation(t);
+    for (size_t sample_index = 0; sample_index < SAMPLE_COUNT; ++sample_index) {
+        float t = float(sample_index) / (SAMPLE_COUNT-1);
+        float b = mipmap.Sample(3,t) / max_brightness_approximation(t);
         // std::cout << std::setw(3) << filtered_value(b) << ',';
-        std::cout << (i == int(std::round(centroid*sample_count)) ? 'X' : '.');
+        std::cout << (sample_index == size_t(std::round(centroid*SAMPLE_COUNT)) ? 'X' : '.');
     }
+    // for (int i = 0; i < mipmap[3].size(); ++i) {
+    //     float t = float(i) / (mipmap[3].size() - 1);
+    //     float b = mipmap[3][i] / max_brightness_approximation(t);
+    //     // std::cout << std::setw(3) << filtered_value(b) << ',';
+    //     std::cout << (i == int(std::round(centroid*sample_count)) ? 'X' : '.');
+    // }
     std::cout << "    " << std::setw(3) << percent(mass) << '\n';
 
     // std::cout << '\n';
