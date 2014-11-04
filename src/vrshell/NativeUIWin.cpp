c81b6ff53a8b15ec6ba7b03e8017cf4beaa858c2
Walter Gray <wgray@leapmotion.com>
2014-11-03 16:27:05 -0800

    Added NativeUI wrapper following the Shortcuts pattern

--- src/vrshell/NativeUIWin.cpp
+++ src/vrshell/NativeUIWin.cpp
@@ -1,2 +1,21 @@
+#include "NativeUI.h"
 #include "NativeUIWin.h"
+
+using namespace VRShell;
+
+NativeUI::NativeUI() {
+
+}
+
+void NativeUI::ShowUI() {
+  NativeUIWin::AddTrayIcon(*this);
+}
+
+void NativeUI::DestroyUI() {
+  Application::Exit();
+}
+
+void NativeUI::ShowToolbarMessage(const char* title, const char* message) {
+  NativeUIWin::ShowToolbarMessage(title, message);
+}
 
