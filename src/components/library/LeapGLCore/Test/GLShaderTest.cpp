7d924fc1b4e43cf077b09aa2a2e9ec449badd442
Victor 'Yes, The Victor' Dods <vdods@leapmotion.com>
2014-11-11 16:11:04 -0800

    Fixed up the disabled GLShader test.

--- src/components/library/LeapGLCore/Test/GLShaderTest.cpp
+++ src/components/library/LeapGLCore/Test/GLShaderTest.cpp
@@ -59,38 +59,43 @@ TEST_F(GLShaderTest, CompileUnsuccessfully) {
   }
 }
 
-TEST_F(GLShaderTest, DISABLED_SetTypedUniformCheckInVertexShader) {
+TEST_F(GLShaderTest, SetTypedUniformCheckInVertexShader_OpenGL_2_1) {
   const auto &type_map = GLShader::OPENGL_2_1_UNIFORM_TYPE_MAP;
   for (auto it = type_map.begin(); it != type_map.end(); ++it) {
     GLenum uniform_type = it->first;
     const std::string &uniform_type_name = it->second;
-//     std::string uniform_variable_name("test_" + uniform_type_name);
-    // Construct a vertex shader source code with one of each type of allowable uniform.
-    std::string vertex_shader_source("#version 120\n");
-    vertex_shader_source += "uniform " + uniform_type_name + " test0;\n";
-    vertex_shader_source += "uniform " + uniform_type_name + " test1;\n";
-    vertex_shader_source +=
-      "void main () {\n"
-      "    bool condition = test0 == test1;\n" // This dumb indirection is to try to get around unused uniforms maybe being compiled out of the shader.
-      "    if (condition) {\n"
-      "        gl_Position = ftransform();\n"
-      "        gl_FrontColor = gl_Color;\n"
-      "    }\n"
-      "}\n";
-    // Dummy fragment shader source
-    std::string fragment_shader_source(
-      "#version 120\n"
-      "void main () {\n"
-      "    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
-      "}\n"
-    );
-    std::cout << vertex_shader_source << '\n';
-
-    // Construct the shader and the GLShaderInterface.
-    std::shared_ptr<GLShader> shader;
-    ASSERT_NO_THROW_(shader = std::make_shared<GLShader>(vertex_shader_source, fragment_shader_source));
-    EXPECT_NO_THROW_(shader->CheckForTypedUniform("test0", uniform_type, VariableIs::REQUIRED));
-    EXPECT_NO_THROW_(shader->CheckForTypedUniform("test1", uniform_type, VariableIs::REQUIRED));
+    // Skip the sampler types for now.
+    if (uniform_type_name.find("sampler") == std::string::npos) {
+      // Construct a vertex shader source code with one of each type of allowable uniform.
+      std::string vertex_shader_source("#version 120\n");
+      vertex_shader_source += "uniform " + uniform_type_name + " test0;\n";
+      vertex_shader_source += "uniform " + uniform_type_name + " test1;\n";
+      vertex_shader_source +=
+        "void main () {\n"
+        "    bool condition = test0 == test1;\n" // This dumb indirection is to try to get around unused uniforms maybe being compiled out of the shader.
+        "    if (condition) {\n"
+        "        gl_Position = ftransform();\n"
+        "        gl_FrontColor = gl_Color;\n"
+        "    }\n"
+        "}\n";
+      // Dummy fragment shader source
+      std::string fragment_shader_source(
+        "#version 120\n"
+        "void main () {\n"
+        "    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
+        "}\n"
+      );
+      std::cout << vertex_shader_source << '\n';
+      // Construct the shader and the GLShaderInterface.
+      std::shared_ptr<GLShader> shader;
+      ASSERT_NO_THROW_(shader = std::make_shared<GLShader>(vertex_shader_source, fragment_shader_source));
+      EXPECT_NO_THROW_(shader->CheckForTypedUniform("test0", uniform_type, VariableIs::REQUIRED));
+      EXPECT_NO_THROW_(shader->CheckForTypedUniform("test1", uniform_type, VariableIs::REQUIRED));
+      EXPECT_NO_THROW_(shader->CheckForTypedUniform("test0", uniform_type, VariableIs::OPTIONAL_NO_WARN));
+      EXPECT_NO_THROW_(shader->CheckForTypedUniform("test1", uniform_type, VariableIs::OPTIONAL_NO_WARN));
+      EXPECT_NO_THROW_(shader->CheckForTypedUniform("test0", uniform_type, VariableIs::OPTIONAL_BUT_WARN));
+      EXPECT_NO_THROW_(shader->CheckForTypedUniform("test1", uniform_type, VariableIs::OPTIONAL_BUT_WARN));
+    }
   }
 }
 
