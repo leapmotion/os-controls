f8b56b6d5d4f25d03869d508746f83a78a119add
Victor 'Yes, The Victor' Dods <vdods@leapmotion.com>
2014-11-20 14:36:00 -0800

    Took deprecated code out of GLShaderMatrices.h/cpp.  Made sure ShaderMatrices is inside Leap::GL namespace.

--- src/components/library/LeapGLCore/Leap/GL/GLShaderMatrices.h
+++ src/components/library/LeapGLCore/Leap/GL/GLShaderMatrices.h
@@ -6,6 +6,9 @@
 #include "Leap/GL/ScopeGuard.h"
 #include "Leap/GL/ShaderFrontend.h"
 
+namespace Leap {
+namespace GL {
+
 // Presents an interface specifically tailored to shaders having [optional] matrix uniforms of the form:
 //   uniform mat4 projection_times_model_view_matrix
 //   uniform mat4 model_view_matrix
@@ -14,55 +17,34 @@
 class ShaderMatrices {
 public:
 
-	// Leaving any of the matrix names empty will cause the corresponding uniforms to go unused.
-	ShaderMatrices (const GLShader &shader,
-		              const std::string &projection_times_model_view_matrix_id,
-		              const std::string &model_view_matrix_id,
-		              const std::string &normal_matrix_id);
+  // Leaving any of the matrix names empty will cause the corresponding uniforms to go unused.
+  ShaderMatrices (const GLShader &shader,
+                  const std::string &projection_times_model_view_matrix_id,
+                  const std::string &model_view_matrix_id,
+                  const std::string &normal_matrix_id);
 
-	void SetMatrices (const EigenTypes::Matrix4x4 &model_view, const EigenTypes::Matrix4x4 &projection);
-	void UploadUniforms ();
+  void SetMatrices (const EigenTypes::Matrix4x4 &model_view, const EigenTypes::Matrix4x4 &projection);
+  void UploadUniforms ();
 
 private:
 
-	enum class ShaderMatrix {
-		PROJECTION_TIMES_MODEL_VIEW,
-		MODEL_VIEW,
-		NORMAL
-	};
+  enum class ShaderMatrix {
+    PROJECTION_TIMES_MODEL_VIEW,
+    MODEL_VIEW,
+    NORMAL
+  };
 
-	template <ShaderMatrix NAME_, GLenum GL_TYPE_, typename CppType_, MatrixStorageConvention MATRIX_STORAGE_CONVENTION_>
-	using ShaderMatrixUniform = Leap::GL::MatrixUniform<ShaderMatrix,NAME_,GL_TYPE_,CppType_,MATRIX_STORAGE_CONVENTION_>;
+  template <ShaderMatrix NAME_, GLenum GL_TYPE_, typename CppType_, MatrixStorageConvention MATRIX_STORAGE_CONVENTION_>
+  using ShaderMatrixUniform = Leap::GL::MatrixUniform<ShaderMatrix,NAME_,GL_TYPE_,CppType_,MATRIX_STORAGE_CONVENTION_>;
 
-	typedef ShaderFrontend<ShaderMatrix,
-				                 ShaderMatrixUniform<ShaderMatrix::PROJECTION_TIMES_MODEL_VIEW,GL_FLOAT_MAT4,EigenTypes::Matrix4x4f,COLUMN_MAJOR>,
-				                 ShaderMatrixUniform<ShaderMatrix::MODEL_VIEW,GL_FLOAT_MAT4,EigenTypes::Matrix4x4f,COLUMN_MAJOR>,
-				                 ShaderMatrixUniform<ShaderMatrix::NORMAL,GL_FLOAT_MAT4,EigenTypes::Matrix4x4f,COLUMN_MAJOR>> Frontend;
-	Frontend m_frontend;
+  typedef ShaderFrontend<ShaderMatrix,
+                         ShaderMatrixUniform<ShaderMatrix::PROJECTION_TIMES_MODEL_VIEW,GL_FLOAT_MAT4,EigenTypes::Matrix4x4f,COLUMN_MAJOR>,
+                         ShaderMatrixUniform<ShaderMatrix::MODEL_VIEW,GL_FLOAT_MAT4,EigenTypes::Matrix4x4f,COLUMN_MAJOR>,
+                         ShaderMatrixUniform<ShaderMatrix::NORMAL,GL_FLOAT_MAT4,EigenTypes::Matrix4x4f,COLUMN_MAJOR>> Frontend;
+  Frontend m_frontend;
 };
-#if 0
-namespace Leap {
-namespace GL {
-namespace GLShaderMatrices {
-  
-// The shader must have the following 4x4 matrix uniforms:
-// - projection_times_model_view_matrix
-// - model_view_matrix
-// - normal_matrix
-// If these uniforms aren't present in the shader with the correct types (GL_FLOAT_MAT4),
-// an exception will be thrown.  The shader may be bound and/or unbound, depending on the
-// value of bind_flags.
-void CheckShaderForUniforms (const GLShader &shader, BindFlags bind_flags);
-
-// All the matrix uniforms in the shader can be derived from the modelview and
-// projection matrices.  This method derives the necessary matrices and uploads
-// them into the uniforms for this shader.  The binding requirement 
-void UploadUniforms (const GLShader &shader, const EigenTypes::Matrix4x4 &model_view, const EigenTypes::Matrix4x4 &projection, BindFlags bind_flags);
-
-} // end of namespace GLShaderMatrices
 
 } // end of namespace GL
 } // end of namespace Leap
 
 using namespace Leap::GL; // TEMPORARY until the Leap::GL:: scoping has been integrated into all code.
-#endif
